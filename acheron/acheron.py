5436b5af6fd49722c0bbef8fb9a1dca11bd9f17a
Ricardo Marliere <rbmarliere@gmail.com>
2020-06-02 23:21:46 -0300

    acheron: fix cleaning function

--- acheron/acheron.py
+++ acheron/acheron.py
@@ -2,76 +2,72 @@ import json
 import nltk
 import re
 import tweepy
-import pprint
 
 # download stopwords from nltk
-nltk.download('stopwords')
+nltk.download("stopwords")
 
 # load config parameters to memory
-with open('config.json') as f:
+with open("config.json") as f:
 	config = json.load(f)
 
-def clean_text(tweet):
+def clean(tweet):
 	# remove links
+	tweet = re.sub("\n", " ", tweet)
 	tweet = re.sub("http\S+", "", tweet)
 	# remove html entities
 	tweet = re.sub("&\w+;", "", tweet)
 	# remove usernames
-	tweet = re.sub("@\w+ ", "", tweet)
-	# leave only letters and numbers
-	tweet = re.sub("[^a-zA-Z0-9]", " ", tweet)
+	tweet = re.sub("(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9-_]+)", "", tweet)
+	# leave only words
+	tweet = re.sub("[^a-zA-Z]", " ", tweet)
 	# convert to lower case, split into individual words
 	words = tweet.lower().split()
-	# remove stopwords
-	keep = [ "until", "while", "between", "above", "below", "up", "down", "over", "under", "here", "there", "when", "both", "more", "will", "should", "should've", "now",  "couldn't", "shouldn", "shouldn,t", "won'", "won't", "wouldn'", "wouldn't" ]
+	# remove stopwords, but keep some
+	keep = [ "until", "while", "between", "above", "below", "up", "down", "over", "under", "here", "there", "when", "both", "more", "will", "should", "should've", "now",  "couldn't", "shouldn", "shouldn't", "won'", "won't", "wouldn'", "wouldn't" ]
 	nltkstops = set(nltk.corpus.stopwords.words("english"))
 	stops = [w for w in nltkstops if not w in keep]
 	meaningful_words = [w for w in words if not w in stops]
 	# stem words
 	stemmer = nltk.stem.porter.PorterStemmer()
 	singles = [stemmer.stem(word) for word in meaningful_words]
-	# join the words back into one string
-	return(" ".join(singles))
-	# todo: spellcheck?
+	# join the words with more than one char back into one string
+	return(" ".join([w for w in singles if len(w) > 1]))
 
 class AcheronListener(tweepy.StreamListener):
 	def on_status(self, status):
 		# ignore retweets
-		if 'retweeted_status' in status._json:
+		if "retweeted_status" in status._json:
 			return
 		# ignore replies
-		#if status._json['in_reply_to_user_id']:
+		#if status._json["in_reply_to_user_id"]:
 		#	return
 		# if tweet is truncated, get all text
-		if 'extended_tweet' in status._json:
-			tweet = status.extended_tweet['full_text']
+		if "extended_tweet" in status._json:
+			tweet = status.extended_tweet["full_text"]
 		else:
 			tweet = status.text
-		# transform tweet into a single line
-		tweet = tweet.replace('\n', ' ').replace('\r', '') + '\n'
-		print(tweet)
 		# clean the tweet text
-		tweet = clean_text(tweet)
+		tweet = clean(tweet)
 		print(tweet)
 		print()
 		print()
-		with open('data', 'a') as output:
+		with open("data", "a") as output:
 			#todo: check if file exists, rename, etc
 			print(tweet, file=output)
 
 # initialize tweepy api object
-auth = tweepy.OAuthHandler(config['CONSUMER_KEY'], config['CONSUMER_SECRET'])
-auth.set_access_token(config['ACCESS_KEY'], config['ACCESS_SECRET'])
+auth = tweepy.OAuthHandler(config["CONSUMER_KEY"], config["CONSUMER_SECRET"])
+auth.set_access_token(config["ACCESS_KEY"], config["ACCESS_SECRET"])
 api = tweepy.API(auth)
 
 # load the ids of the users to track into memory
 try:
-	with open('uids') as uids:
+	with open("uids") as uids:
 		users = uids.read().splitlines()
 except FileNotFoundError:
 	users = []
-	with open('uids', 'a') as uids:
-		for user in config['TRACK_USERS']:
+	with open("uids", "a") as uids:
+		for user in config["TRACK_USERS"]:
 			# retrieve user id by name from twitter api
 			uid = str(api.get_user(user.strip()).id)
 			print(uid, file=uids)
@@ -80,5 +76,5 @@ except FileNotFoundError:
 acheron = AcheronListener()
 stream = tweepy.Stream(auth = api.auth, listener=acheron)
 #stream.filter(languages=["en"], follow=users, is_async=True)
-stream.filter(languages=["en"], track=['bitcoin'], is_async=True)
+stream.filter(languages=["en"], track=["bitcoin"], is_async=True)
 
