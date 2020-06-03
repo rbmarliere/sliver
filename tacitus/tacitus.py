76cd624e39b185b4805080a48339defe8ade0130
Ricardo Marliere <rbmarliere@gmail.com>
2020-06-03 19:02:39 -0300

    tacitus: refactor to use word2vec embeddings

--- tacitus/tacitus.py
+++ tacitus/tacitus.py
@@ -1,109 +1,121 @@
-# https://keras.io/examples/nlp/text_classification_from_scratch/
-
-from tensorflow.keras import layers
-from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
 import datetime
-import re
-import tensorflow
-import sys
+import gensim
+import logging
 import numpy
+import numpy
+import pandas
+import re
+import sys
+import tensorflow
 
-# load data
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+# declare hyperparameters
+validation_split = 0.2
+embedding_dim = 33
+epochs = 3
 batch_size = 10
 
-raw_train_ds = tensorflow.keras.preprocessing.text_dataset_from_directory(
-	"train",
-	batch_size=batch_size,
-	validation_split=0.2,
-	subset="training",
-	seed=1337,
-)
-print(
-	"Number of batches in raw_train_ds: %d"
-	% tensorflow.data.experimental.cardinality(raw_train_ds)
-)
+# load relevant tweets
+relevant_file = open("train/relevant")
+relevant_lines = relevant_file.read().splitlines()
+relevant = pandas.DataFrame({ 'tweet': relevant_lines, 'is_relevant': [1] * len(relevant_lines) })
 
-raw_val_ds = tensorflow.keras.preprocessing.text_dataset_from_directory(
-	"train",
-	batch_size=batch_size,
-	validation_split=0.2,
-	subset="validation",
-	seed=1337,
-)
-print(
-	"Number of batches in raw_val_ds: %d" % tensorflow.data.experimental.cardinality(raw_val_ds)
-)
+# load irrelevant tweets
+irrelevant_file = open("train/irrelevant")
+irrelevant_lines = irrelevant_file.read().splitlines()
+irrelevant = pandas.DataFrame({ 'tweet': irrelevant_lines, 'is_relevant': [0] * len(irrelevant_lines) })
 
-#raw_test_ds = tensorflow.keras.preprocessing.text_dataset_from_directory(
-#	"test", batch_size=batch_size
-#)
-#print(
-#	"Number of batches in raw_test_ds: %d"
-#	% tensorflow.data.experimental.cardinality(raw_test_ds)
-#)
+# combine all data
+raw_data = pandas.concat([relevant, irrelevant])
 
-# model constants
-max_features = 6666
-embedding_dim = 33
-sequence_length = 112
+# shuffle rows
+raw_data = raw_data.sample(frac=1).reset_index(drop=True)
 
-# vocab layer
-vectorize_layer = TextVectorization(
-	max_tokens=max_features,
-	output_mode="int",
-	output_sequence_length=sequence_length,
-)
+# build vocab
+tweets = raw_data['tweet'].values.tolist()
+wvmodel = gensim.models.word2vec.Word2Vec(tweets, size=embedding_dim, window=10, min_count=5, workers=16, sg=1) # sg=1 is skipgram, default is 0 = cbow
+wvmodel.wv.save_word2vec_format("corpus", binary=False)
 
-# create vocab
-text_ds = raw_train_ds.map(lambda x, y: x)
-vectorize_layer.adapt(text_ds)
+# load vocab
+embeddings_index = {}
+f = open("corpus")
+for line in f:
+	values = line.split()
+	word = values[0]
+	coefs = numpy.asarray(values[1:])
+	embeddings_index[word] = coefs
 
 # vectorize data
-def vectorize_text(text, label):
-	text = tensorflow.expand_dims(text, -1)
-	return vectorize_layer(text), label
-train_ds = raw_train_ds.map(vectorize_text)
-val_ds = raw_val_ds.map(vectorize_text)
-#test_ds = raw_test_ds.map(vectorize_text)
-# Do async prefetching / buffering of the data for best performance on GPU.
-train_ds = train_ds.cache().prefetch(buffer_size=10)
-val_ds = val_ds.cache().prefetch(buffer_size=10)
-#test_ds = test_ds.cache().prefetch(buffer_size=10)
+tok = tensorflow.keras.preprocessing.text.Tokenizer()
+tok.fit_on_texts(tweets)
+seq = tok.texts_to_sequences(tweets)
+
+maxlen = max([len(s.split()) for s in tweets])
+
+tweets_pad = tensorflow.python.keras.preprocessing.sequence.pad_sequences(seq, maxlen=maxlen)
+is_relevant = raw_data['is_relevant'].values
+
+num_words = len(tok.word_index) + 1
+embedding_matrix = numpy.zeros((num_words, embedding_dim))
+for word, i in tok.word_index.items():
+	if i > num_words:
+		continue
+	embedding_vector = embeddings_index.get(word)
+	if embedding_vector is not None:
+		# words not found will be all-zeros
+		embedding_matrix[i] = embedding_vector
 
 # build model
-# A integer input for vocab indices.
 inputs = tensorflow.keras.Input(shape=(None,), dtype="int64")
-#inputs = tensorflow.keras.Input(shape=(1,), dtype=tensorflow.string, name='text')
-# Next, we add a layer to map those vocab indices into a space of dimensionality
 # "embedding_dim".
-x = layers.Embedding(max_features, embedding_dim)(inputs)
-x = layers.Dropout(0.5)(x)
+x = tensorflow.keras.layers.Embedding(
+		num_words,
+		embedding_dim,
+		embeddings_initializer=tensorflow.keras.initializers.Constant( embedding_matrix ),
+		input_length=maxlen,
+		trainable=False
+	)(inputs)
+x = tensorflow.keras.layers.Dropout(0.5)(x)
 # Conv1D + global max pooling
-x = layers.Conv1D(128, 7, padding="valid", activation="relu", strides=3)(x)
-x = layers.Conv1D(128, 7, padding="valid", activation="relu", strides=3)(x)
-x = layers.GlobalMaxPooling1D()(x)
+x = tensorflow.keras.layers.Conv1D(128, 7, padding="valid", activation="relu", strides=3)(x)
+x = tensorflow.keras.layers.Conv1D(128, 7, padding="valid", activation="relu", strides=3)(x)
+x = tensorflow.keras.layers.GlobalMaxPooling1D()(x)
 # We add a vanilla hidden layer:
-x = layers.Dense(128, activation="relu")(x)
-x = layers.Dropout(0.5)(x)
+x = tensorflow.keras.layers.Dense(128, activation="relu")(x)
+x = tensorflow.keras.layers.Dropout(0.5)(x)
 # We project onto a single unit output layer, and squash it with a sigmoid:
-predictions = layers.Dense(1, activation="sigmoid", name="predictions")(x)
+predictions = tensorflow.keras.layers.Dense(1, activation="sigmoid", name="predictions")(x)
 model = tensorflow.keras.Model(inputs, predictions)
 # Compile the model with binary crossentropy loss and an adam optimizer.
 model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
 
-# train model
-epochs = 13
-model.fit(train_ds, validation_data=val_ds, epochs=epochs)
+print(model.summary())
 
-# create e2e model that receives raw strings as input
-inputs = tensorflow.keras.Input(shape=(1,), dtype="string")
-indices = vectorize_layer(inputs)
-outputs = model(indices)
-end_to_end_model = tensorflow.keras.Model(inputs, outputs)
-end_to_end_model.compile(
-	loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"]
+# train model
+nsamples = int(validation_split * raw_data.size)
+x_train = tweets_pad[:-nsamples]
+y_train = is_relevant[:-nsamples]
+x_test  = tweets_pad[-nsamples:]
+y_test  = is_relevant[-nsamples:]
+
+model.fit(
+	x_train,
+	y_train,
+	validation_data=(x_test, y_test),
+	batch_size=batch_size,
+	epochs=epochs
 )
 
+# create e2e model that receives raw strings as input
+#inputs = tensorflow.keras.Input(shape=(1,), dtype="string")
+#indices = vectorize_layer(inputs)
+#outputs = model(indices)
+#end_to_end_model = tensorflow.keras.Model(inputs, outputs)
+#end_to_end_model.compile(
+#	loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"]
+#)
+
 # save e2e model
-end_to_end_model.save("model")
+#end_to_end_model.save("model")
 
