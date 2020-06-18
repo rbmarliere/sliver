79b1862240a32cf54cc46997f8b31e41a8ac124b
Ricardo Marliere <rbmarliere@gmail.com>
2020-06-18 00:38:05 -0300

    acheron: first tally implementation

--- acheron/acheron.py
+++ acheron/acheron.py
@@ -37,10 +37,54 @@ with open(args.config) as f:
 # load temporal markers
 temporal = open("temporal").read().splitlines()
 
+# helper to map hours of day into 4h periods
+def get_period(hour):
+	hours_to_periods = {
+		0:0, 1:0, 2:0, 3:0,
+		4:1, 5:1, 6:1, 7:1,
+		8:2, 9:2, 10:2, 11:2,
+		12:3, 13:3, 14:3, 15:3,
+		16:4, 17:4, 18:4, 19:4,
+		20:5, 21:5, 22:5, 23:5
+	}
+	return hours_to_periods.get(hour)
+
+# helper to compute sentiment over a stream of tweets
+def tally(period):
+	inputfile = "data/" + period + ".txt"
+	outputfile = "data/" + period + "_tally.txt"
+
+	try:
+		with open(inputfile) as inp:
+			scores = [ re.findall(r'>>> SCORE: (.+)', line) for line in inp ]
+			scores = [ x for x in scores if x != [] ]
+	except FileNotFoundError:
+		return
+
+	if scores == []:
+		return
+
+	pos = neg = 0
+	for score in scores:
+		if float(score[0]) <= 0.65:
+			pos += 1
+		elif float(score[0]) >= 0.25:
+			neg += 1
+
+	with open(outputfile, "w") as out:
+		print("Positives: " + str(pos), file=out)
+		print("Negatives: " + str(neg), file=out)
+		print("Result: " + str(pos - neg > 0), file=out)
+
 class AcheronListener(tweepy.StreamListener):
 	def __init__(self):
 		super().__init__()
+
 		self.tacitus = tensorflow.keras.models.load_model(args.model)
+
+		# tally period tracker
+		now = datetime.datetime.now()
+		self.last_period = now.strftime("%Y%m%d") + "-" + str( get_period(now.hour) )
 
 	def on_status(self, status):
 		# ignore retweets
@@ -58,22 +102,37 @@ class AcheronListener(tweepy.StreamListener):
 
 		print("\n" + tweet)
 		for w in temporal:
+			# ignore tweets that doesnt contain temporal words
 			if w in tweet:
-				pred = self.tacitus.predict(use([tweet]))[0][1]
-				score = format(pred, 'f')
-				print(">>> SCORE: " + score + "\n")
+				# predict relevant tweet using tacitus
+			pred = self.tacitus.predict(use([tweet]))[0][1]
+			# record result
+			score = format(pred, 'f')
+			print("SCORE: " + score + "\n")
 
-				created_at = status._json["created_at"]
-				user = status._json["user"]["screen_name"]
-				url = "https://twitter.com/" + user + "/status/" + str(status._json["id"])
+			# parse tweet info
+			created_at = status._json["created_at"]
+			user = status._json["user"]["screen_name"]
+			url = "https://twitter.com/" + user + "/status/" + str(status._json["id"])
 
-				today = datetime.datetime.now().strftime("%Y%m%d")
-				with open("data/" + today + ".txt", "a") as out:
-					print(created_at, file=out)
-					print(user, file=out)
-					print(url, file=out)
-					print(tweet, file=out)
-					print(">>> SCORE: " + score + "\n", file=out)
+			# compute output file name
+			now = datetime.datetime.now()
+			period = now.strftime("%Y%m%d") + "-" + str( get_period(now.hour) )
+
+			# check if period ended
+			if period != self.last_period:
+				# if so, compute sentiment of last period stream
+				tally(self.last_period)
+
+			with open("data/" + period + ".txt", "a") as out:
+				print(created_at, file=out)
+				print(user, file=out)
+				print(url, file=out)
+				print(tweet, file=out)
+				print(">>> SCORE: " + score + "\n", file=out)
+
+			# take note of current period
+			self.last_period = period
 
 # initialize tweepy api object
 auth = tweepy.OAuthHandler(config["CONSUMER_KEY"], config["CONSUMER_SECRET"])
@@ -108,10 +167,11 @@ stream = tweepy.Stream(auth = api.auth, listener=acheron, timeout=600)
 
 while not stream.running:
 	try:
+		logging.info("Streaming...")
 		stream.filter(
 			languages=["en"],
-			#follow=users,
-			track=[ "bitcoin", "btc", "xbt" ],
+			follow=users,
+			#track=[ "bitcoin", "btc", "xbt" ],
 			is_async=False
 		)
 	except (Timeout, SSLError, ReadTimeoutError, ConnectionError) as e:
