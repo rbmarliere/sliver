bdcc9445969bb303df5efbe5e6c0f51460ef1931
Ricardo Marliere <rbmarliere@gmail.com>
2020-07-14 02:12:36 -0300

    vader

--- clean.sh
+++ clean.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-rm data/{filter,parse,predict,split,tally}/*
+rm data/{filter,parse,predict,split,tally,vader}/*
 ./hypnox filter --input data/raw
 ./hypnox split --input data/filter --timeframe 4h
-./hypnox parse --input data/split
-./hypnox predict --input data/split
-./hypnox tally --input data/predict
+./hypnox vader --input data/split
+./hypnox parse --input data/vader
+#./hypnox predict --input data/split
+#./hypnox tally --input data/predict
 
