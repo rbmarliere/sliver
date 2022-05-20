8950662b674d74d57aaf6af37b5e2ef3d40e6c90
Ricardo Marliere <rbmarliere@gmail.com>
2022-05-20 12:32:29 -0300

    refactor + 20220520 model and training data

--- hypnox.py
+++ hypnox.py
@@ -24,90 +24,6 @@ except LookupError:
 nltkstops = set(nltk.corpus.stopwords.words("english"))
 stops = [w for w in nltkstops if not w in config["TO_KEEP_STOPWORDS"]]
 
-class Stream(tweepy.Stream):
-	def on_status(self, status):
-		# ignore retweets
-		if "retweeted_status" in status._json:
-			return
-		# ignore replies
-		if status._json["in_reply_to_user_id"]:
-			return
-		# if tweet is truncated, get all text
-		if "extended_tweet" in status._json:
-			text = status.extended_tweet["full_text"]
-		else:
-			text = status.text
-		# make the tweet single-line
-		text = re.sub("\n", " ", text)
-		# parse tweet info
-		created_at = datetime.datetime.strptime(status._json["created_at"], "%a %b %d %H:%M:%S %z %Y")
-		user = status._json["user"]["screen_name"]
-		url = "https://twitter.com/" + user + "/status/" + str(status.id)
-		# output
-		tweet = pandas.DataFrame({ "date": [created_at], "username": [user], "url": [url], "tweet": [text] })
-		logging.info(text)
-		with open("data/raw.csv", "a") as f:
-			tweet.to_csv(f, header=f.tell()==0, mode="a", index=False)
-
-# save the ids of the users to track to disk
-def save_uids(users, api):
-	logging.info("loading user ids")
-	uids_file = open(".uids", "a")
-	uids = []
-	for user in users:
-		# retrieve user id by name from twitter api
-		logging.info("fetching user %s id" % user)
-		try:
-			uid = str(api.get_user(screen_name=user.strip()).id)
-			# save found uids to a file so it doesn't consume api each run
-			print(uid, file=uids_file)
-			uids.append(uid)
-		except tweepy.errors.TweepyException:
-			logging.error("user '" + user + "' not found")
-	return uids
-
-def stream(argp, args):
-	logging.info("loading config")
-	if config["TRACK_USERS"] == "":
-		logging.error("empty user list in config! (TRACK_USERS) in (" + config_filename + ")")
-		return 1
-
-	logging.info("loading twitter API keys")
-	if config["CONSUMER_KEY"] == "" or config["CONSUMER_SECRET"] == "" or config["ACCESS_KEY"] == "" or config["ACCESS_SECRET"] == "":
-		logging.error("empty keys in config! (CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)")
-		return 1
-	auth = tweepy.OAuthHandler(config["CONSUMER_KEY"], config["CONSUMER_SECRET"])
-	auth.set_access_token(config["ACCESS_KEY"], config["ACCESS_SECRET"])
-	api = tweepy.API(auth)
-
-	logging.info("initializing")
-	stream = Stream(config["CONSUMER_KEY"], config["CONSUMER_SECRET"], config["ACCESS_KEY"], config["ACCESS_SECRET"])
-
-	logging.info("reading users")
-	try:
-		uids = open(".uids").read().splitlines()
-		if len(uids) != len(config["TRACK_USERS"]):
-			os.remove(".uids")
-			uids = save_uids(config["TRACK_USERS"], api)
-	except FileNotFoundError:
-		uids = save_uids(config["TRACK_USERS"], api)
-
-	while not stream.running:
-		try:
-			stream.filter(
-				languages=["en"],
-				follow=uids,
-				#track=["bitcoin"], -> need to filter bots by follower count
-			)
-		except (requests.exceptions.Timeout, ssl.SSLError, urllib3.exceptions.ReadTimeoutError, requests.exceptions.ConnectionError) as e:
-			logging.error("network error")
-		except Exception as e:
-			logging.error("unexpected error", e)
-		except KeyboardInterrupt:
-			return 1
-		finally:
-			logging.error("stream has crashed")
-
 def standardize(tweet):
 	# convert to lower case
 	tweet = tensorflow.strings.lower(tweet)
@@ -139,6 +55,102 @@ def standardize(tweet):
 #				tweet.flat_values) )
 #	return tweet
 
+class Stream(tweepy.Stream):
+	def on_status(self, status):
+		# ignore retweets
+		if "retweeted_status" in status._json:
+			return
+		# ignore replies
+		if status._json["in_reply_to_user_id"]:
+			return
+		# if tweet is truncated, get all text
+		if "extended_tweet" in status._json:
+			text = status.extended_tweet["full_text"]
+		else:
+			text = status.text
+		# make the tweet single-line
+		text = re.sub("\n", " ", text)
+		# parse tweet info
+		created_at = datetime.datetime.strptime(status._json["created_at"], "%a %b %d %H:%M:%S %z %Y")
+		user = status._json["user"]["screen_name"]
+		url = "https://twitter.com/" + user + "/status/" + str(status.id)
+		# is predictive tweet?
+		is_prediction = self.model.predict([text])[0][0]
+		# output
+		tweet = pandas.DataFrame({ "date": [created_at], "username": [user], "url": [url], "tweet": [text], "is_prediction": [is_prediction], "model": [self.modelname] })
+		logging.info(text)
+		logging.info("is_prediction: " + str(is_prediction))
+		with open("data/raw.csv", "a") as f:
+			tweet.to_csv(f, header=f.tell()==0, mode="a", index=False)
+
+# save the ids of the users to track to disk
+def save_uids(users, api):
+	logging.info("loading user ids")
+	uids_file = open(".uids", "a")
+	uids = []
+	for user in users:
+		# retrieve user id by name from twitter api
+		logging.info("fetching user %s id" % user)
+		try:
+			uid = str(api.get_user(screen_name=user.strip()).id)
+			# save found uids to a file so it doesn't consume api each run
+			print(uid, file=uids_file)
+			uids.append(uid)
+		except tweepy.errors.TweepyException:
+			logging.error("user '" + user + "' not found")
+	return uids
+
+def stream(argp, args):
+	logging.info("loading config")
+	if config["TRACK_USERS"] == "":
+		logging.error("empty user list in config! (TRACK_USERS) in (" + config_filename + ")")
+		return 1
+	if args.model == None:
+		logging.error("provide a model name with --model")
+		return 1
+	modelpath = os.path.dirname(os.path.realpath(__file__)) + "/models/" + args.model
+	if not os.path.exists(modelpath):
+		logging.warning(modelpath + " not found")
+		return 1
+
+	logging.info("loading twitter API keys")
+	if config["CONSUMER_KEY"] == "" or config["CONSUMER_SECRET"] == "" or config["ACCESS_KEY"] == "" or config["ACCESS_SECRET"] == "":
+		logging.error("empty keys in config! (CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)")
+		return 1
+	auth = tweepy.OAuthHandler(config["CONSUMER_KEY"], config["CONSUMER_SECRET"])
+	auth.set_access_token(config["ACCESS_KEY"], config["ACCESS_SECRET"])
+	api = tweepy.API(auth)
+
+	logging.info("initializing")
+	stream = Stream(config["CONSUMER_KEY"], config["CONSUMER_SECRET"], config["ACCESS_KEY"], config["ACCESS_SECRET"])
+	stream.model = tensorflow.keras.models.load_model(modelpath, custom_objects={"standardize": standardize})
+	stream.modelname = args.model
+
+	logging.info("reading users")
+	try:
+		uids = open(".uids").read().splitlines()
+		if len(uids) != len(config["TRACK_USERS"]):
+			os.remove(".uids")
+			uids = save_uids(config["TRACK_USERS"], api)
+	except FileNotFoundError:
+		uids = save_uids(config["TRACK_USERS"], api)
+
+	while not stream.running:
+		try:
+			stream.filter(
+				languages=["en"],
+				follow=uids,
+				#track=["bitcoin"], -> need to filter bots by follower count
+			)
+		except (requests.exceptions.Timeout, ssl.SSLError, urllib3.exceptions.ReadTimeoutError, requests.exceptions.ConnectionError) as e:
+			logging.error("network error")
+		except Exception as e:
+			logging.error("unexpected error", e)
+		except KeyboardInterrupt:
+			return 1
+		finally:
+			logging.error("stream has crashed")
+
 def train(argp, args):
 	if args.model == None:
 		logging.error("provide a model name with --model")
@@ -153,14 +165,19 @@ def train(argp, args):
 	if os.path.exists(modelpath):
 		logging.warning(modelpath + " already exists, overwrite? [y|N]")
 		if input() != "y":
-			exit
+			return 1
 		shutil.rmtree(modelpath)
+	modelcfgpath = os.path.dirname(os.path.realpath(__file__)) + "/models/" + args.model + ".conf"
+	if not os.path.exists(modelcfgpath):
+		logging.warning(modelcfg + " not found")
+		return 1
 
-	batch_size = 16
-	max_features = 20000
-	sequence_length = 280
-	embedding_dim = 32
-	epochs = 11
+	modelcfg = json.load(open(modelcfgpath))
+	batch_size = modelcfg["BATCH_SIZE"]
+	max_features = modelcfg["MAX_FEATURES"]
+	sequence_length = modelcfg["SEQUENCE_LENGTH"]
+	embedding_dim = modelcfg["EMBEDDING_DIM"]
+	epochs = modelcfg["EPOCHS"]
 
 	raw_df = pandas.read_csv(args.input, encoding="utf-8", lineterminator="\n")
 
@@ -203,8 +220,7 @@ def train(argp, args):
 		optimizer='adam',
 		metrics=tensorflow.metrics.BinaryAccuracy(threshold=0.0))
 
-	log_dir = "models/logs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-	tensorboard_callback = tensorflow.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+	tensorboard_callback = tensorflow.keras.callbacks.TensorBoard(log_dir=modelpath+"/logs", histogram_freq=1)
 	history = model.fit(train_ds, validation_data=val_ds, epochs=epochs, callbacks=[tensorboard_callback])
 
 	loss, accuracy = model.evaluate(test_ds)
