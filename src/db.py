e50417da8ee28bca4cf43753ccf57bba69017df5
Ricardo Marliere <rbmarliere@gmail.com>
2022-07-18 21:00:14 -0300

    optimize replay

--- src/db.py
+++ src/db.py
@@ -1,4 +1,3 @@
-import concurrent
 import logging
 import os
 import psycopg2
@@ -12,9 +11,10 @@ def init():
 	cursor = db.cursor()
 	return db, cursor
 
-def predict(model, tweets):
-	intensities = [ "{:.8f}".format(x[0]) for x in model.predict(tweets) ]
-	return intensities
+def close(db, c):
+	db.commit()
+	c.close()
+	db.close()
 
 def replay(argp, args):
 	if args.model == None:
@@ -25,34 +25,18 @@ def replay(argp, args):
 		logging.warning(modelpath + " not found")
 		return 1
 
-	cpu_count = os.cpu_count()
-	sector_size = 1024
-	page_size = sector_size * cpu_count
 	model = tensorflow.keras.models.load_model(modelpath, custom_objects={"standardize": src.standardize.standardize})
 	db, c = init()
 
-	c.execute("SELECT * FROM stream_user WHERE model IS NULL OR model <> '" + args.model + "'")
-	while True:
-		rows = c.fetchmany(page_size)
-		if not rows:
-			break
-		#model.predict([ row[2] for row in rows ], verbose=1, use_multiprocessing=True, workers=cpu_count)
+	c.execute("SELECT * FROM stream_user WHERE model IS NULL OR model <> '" + args.model + "' ORDER BY id ASC")
+	rows = c.fetchall()
+	ids = [ row[0] for row in rows ]
+	tweets = [ row[2] for row in rows ]
+	intensities = [ "{:.8f}".format(x[0]) for x in model.predict(tweets, verbose=1, use_multiprocessing=True, workers=os.cpu_count) ]
+	for tweet_id, tweet_intensity in zip(ids, intensities):
+		db.cursor().execute("UPDATE stream_user SET intensity = %s, model = %s WHERE id = %s", (tweet_intensity, args.model, tweet_id))
 
-		with concurrent.futures.ThreadPoolExecutor(max_workers=cpu_count) as executor:
-			ids = [ row[0] for row in rows ]
-			tweets = [ row[2] for row in rows ]
-			futures = {}
-			for i in range(0, len(rows), sector_size):
-				futures[i] = { executor.submit(predict, model, tweets[i:i+sector_size]) }
-			for i, f in futures.items():
-				for future in concurrent.futures.as_completed(f):
-					intensities = future.result()
-					for tweet_id, tweet_intensity in zip(ids[i:i+sector_size], intensities):
-						db.cursor().execute("UPDATE stream_user SET intensity = %s, model = %s WHERE id = %s", (tweet_intensity, args.model, tweet_id))
-
-	db.commit()
-	c.close()
-	db.close()
+	close(db, c)
 
 def store(argp, args):
 	if args.input == None:
@@ -67,14 +51,10 @@ def store(argp, args):
 	with open(args.input, "r", encoding="utf-8") as f:
 		c.copy_expert(sql, f)
 
-	db.commit()
-	c.close()
-	db.close()
+	close(db, c)
 
 def insert(tweet):
 	db, c = init()
 	sql = "INSERT INTO stream_user(created_at,text) VALUES(%s, %s)"
 	c.execute(sql, (tweet["created_at"], tweet["text"]))
-	db.commit()
-	c.close()
-	db.close()
+	close(db, c)
