59b9a6f9cd94cbb15f6a606e13e4b0934f6e0cc1
Ricardo Marliere <rbmarliere@gmail.com>
2022-07-19 22:49:53 -0300

    faster replay + 20220719_i3 model

--- src/db.py
+++ src/db.py
@@ -8,6 +8,7 @@ import tensorflow
 def init():
 	config = src.config.Config()
 	db = psycopg2.connect(host=config.config["DB_HOST"], database=config.config["DB_DATABASE"], user=config.config["DB_USER"], password=config.config["DB_PASSWORD"])
+	# TODO check connection
 	cursor = db.cursor()
 	return db, cursor
 
@@ -31,6 +32,7 @@ def replay(argp, args):
 		modelcol = "model_i"
 		target = "intensity"
 	if args.table:
+		# TODO check if table exists
 		table = args.table
 	else:
 		table = "stream_user"
@@ -38,13 +40,13 @@ def replay(argp, args):
 	model = tensorflow.keras.models.load_model(modelpath, custom_objects={"standardize": src.standardize.standardize})
 	db, c = init()
 
-	c.execute("SELECT * FROM \"%s\" WHERE %s IS NULL OR %s <> '%s' ORDER BY id ASC" % (table, modelcol, modelcol, args.model))
+	c.execute("SELECT * FROM \"%s\" WHERE %s IS NULL OR %s <> '%s' ORDER BY id ASC limit 3" % (table, modelcol, modelcol, args.model))
 	rows = c.fetchall()
 	ids = [ row[0] for row in rows ]
 	tweets = [ row[2] for row in rows ]
-	scores = [ "{:.8f}".format(x[0]) for x in model.predict(tweets, verbose=1, use_multiprocessing=True, workers=os.cpu_count) ]
-	for tweet_id, tweet_score in zip(ids, scores):
-		db.cursor().execute("UPDATE \"%s\" SET %s = %s, %s = '%s' WHERE id = %s" % (table, target, tweet_score, modelcol, args.model, tweet_id))
+	scores = [ x[0] for x in model.predict(tweets, verbose=1, use_multiprocessing=True, workers=os.cpu_count()) ]
+	tuples = str(list(zip( ids, scores, [args.model] * len(ids) )))[1:-1]
+	c.execute("UPDATE \"%s\" AS t SET %s = t2.%s, %s = t2.%s from (values %s) as t2(id,%s,%s) where t2.id = t.id" % (table, target, target, modelcol, modelcol, tuples, target, modelcol) )
 
 	close(db, c)
 
