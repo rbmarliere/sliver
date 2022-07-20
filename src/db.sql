2bc349c9f54e036bacbcd03770f4494a299cdbe2
Ricardo Marliere <rbmarliere@gmail.com>
2022-07-19 22:49:53 -0300

    faster replay + 20220719_i3 model

--- src/db.sql
+++
@@ -1,4 +0,0 @@
-create database hypnox;
-create user hypnox with password '';
-grant all privileges on database "hypnox" to hypnox;
-create table stream_user (id serial primary key, created_at timestamp, text varchar, model varchar, intensity decimal, polarity decimal)
