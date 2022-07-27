599cf602b0b7dd0a0219647d0ed7bab252ea182d
Ricardo Marliere <rbmarliere@gmail.com>
2022-07-27 20:44:48 -0300

    finbert

--- src/model.py
+++ src/model.py
@@ -6,6 +6,10 @@ import src.standardize
 import tensorflow
 import yaml
 
+from sklearn.model_selection import train_test_split
+from transformers import BertTokenizer, TFBertForSequenceClassification, pipeline
+from datasets import Dataset
+
 def predict(argp, args):
     if args.model == None:
         logging.error("provide a model name with --model")
@@ -139,5 +143,107 @@ def train(argp, args):
     export_model.summary()
     export_model.save(modelpath)
 
-def train_B(argp, args):
-    print("BERT")
+def predictb(argp, args):
+    if args.model == None:
+        logging.error("provide a model name with --model")
+        return 1
+    modelpath = "models/" + args.model
+    if not os.path.exists(modelpath):
+        logging.warning(modelpath + " not found")
+        return 1
+    if args.input == None:
+        logging.error("provide an input data .tsv file name with --input")
+        return 1
+    if not os.path.exists(args.input):
+        logging.warning(args.input + " not found")
+        return 1
+
+    class ListDataset(Dataset):
+        def __init__(self, original_list):
+            self.original_list = original_list
+
+        def __len__(self):
+            return len(self.original_list)
+
+        def __getitem__(self, i):
+            return self.original_list[i]
+
+    model = TFBertForSequenceClassification.from_pretrained(modelpath)
+    tokenizer = BertTokenizer.from_pretrained(modelpath + "/tokenizer")
+    nlp = pipeline("text-classification", model=model, tokenizer=tokenizer)
+    labels = {"Neutral": 0, "Negative": -1, "Positive": 1}
+    df = pandas.read_csv(args.input, lineterminator="\n", encoding="utf-8", sep="\t")
+    df["clean_tweet"] = df["tweet"].apply(src.standardize.clean)
+    df = df.dropna()
+    df["polarity"] = [ labels[i["label"]] for i in nlp(df["clean_tweet"].values.tolist()) ]
+    df["intensity"] = df["intensity"].apply("{:.8f}".format)
+    output = "data/predict/" + args.model + ".tsv"
+    df.to_csv(output, index=False, sep="\t")
+    logging.info("saved output data to " + output)
+
+def trainb(argp, args):
+    if args.input == None:
+        logging.error("provide a training data .tsv file name with --input")
+        return 1
+    if not os.path.exists(args.input):
+        logging.warning(args.input + " not found")
+        return 1
+    if args.model == None:
+        logging.error("provide a model name with --model")
+        return 1
+    modelpath = "models/" + args.model
+    if os.path.exists(modelpath):
+        logging.warning(modelpath + " already exists, overwrite? [y|N]")
+        if input() != "y":
+            return 1
+        shutil.rmtree(modelpath)
+    modelcfgpath = "models/" + args.model + ".yaml"
+    if not os.path.exists(modelcfgpath):
+        logging.warning(modelcfgpath + " not found")
+        return 1
+
+    with open(modelcfgpath, "r") as stream:
+        try:
+            modelcfg = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+            return 1
+    batch_size = modelcfg["batch_size"]
+    sequence_length = modelcfg["sequence_length"]
+    epochs = modelcfg["epochs"]
+    learning_rate = modelcfg["learning_rate"]
+    patience = modelcfg["patience"]
+    min_delta = modelcfg["min_delta"]
+
+    model = TFBertForSequenceClassification.from_pretrained('yiyanghkust/finbert-tone', num_labels=3, from_pt=True)
+    tokenizer = BertTokenizer.from_pretrained('yiyanghkust/finbert-tone')
+
+    raw_df = pandas.read_csv(args.input, encoding="utf-8", lineterminator="\n", sep="\t")
+    raw_df["sentence"] = raw_df["sentence"].apply(src.standardize.clean)
+    raw_df = raw_df.dropna()
+    df_train, df_test, = train_test_split(raw_df, stratify=raw_df['label'], test_size=0.1, random_state=93)
+    df_train, df_val = train_test_split(df_train, stratify=df_train['label'],test_size=0.1, random_state=93)
+    dataset_train = Dataset.from_pandas(df_train)
+    dataset_val = Dataset.from_pandas(df_val)
+    dataset_test = Dataset.from_pandas(df_test)
+    dataset_train = dataset_train.map(lambda e: tokenizer(e['sentence'], truncation=True, padding='max_length', max_length=sequence_length), batched=True)
+    dataset_val = dataset_val.map(lambda e: tokenizer(e['sentence'], truncation=True, padding='max_length', max_length=sequence_length), batched=True)
+    dataset_test = dataset_test.map(lambda e: tokenizer(e['sentence'], truncation=True, padding='max_length', max_length=sequence_length), batched=True)
+    train_ds = dataset_train.to_tf_dataset(columns=['input_ids', 'token_type_ids', 'attention_mask'],
+    label_cols=["label"], batch_size=batch_size)
+    val_ds = dataset_val.to_tf_dataset(columns=['input_ids', 'token_type_ids', 'attention_mask'], label_cols=["label"], batch_size=batch_size)
+    test_ds = dataset_test.to_tf_dataset(columns=['input_ids', 'token_type_ids', 'attention_mask'], label_cols=["label"], batch_size=batch_size)
+
+    optimizer = tensorflow.keras.optimizers.Adam(learning_rate=learning_rate)
+    loss = tensorflow.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    metric = tensorflow.keras.metrics.SparseCategoricalAccuracy('accuracy')
+    model.compile(optimizer=optimizer, loss=loss, metrics=[metric])
+
+    callback = tensorflow.keras.callbacks.EarlyStopping(monitor='accuracy', patience=patience, min_delta=min_delta)
+    tensorboard_callback = tensorflow.keras.callbacks.TensorBoard(log_dir=modelpath + "/logs", histogram_freq=1)
+    history = model.fit( train_ds, validation_data=val_ds, epochs=epochs, callbacks=[tensorboard_callback],
+    batch_size=batch_size)
+
+    model.save_pretrained(modelpath)
+    tokenizer.save_pretrained(modelpath + "/tokenizer")
+
--- src/standardize.py
+++ src/standardize.py
@@ -3,14 +3,21 @@ import re
 import string
 import tensorflow
 
-TO_KEEP_STOPWORDS = [ "this", "that'll", "these", "having", "does", "doing", "until", "while", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "few", "both", "more", "most", "other", "some", "than", "too", "very", "can", "will", "should", "should've", "now", "ain", "aren", "aren't", "could", "couldn", "couldn't", "didn", "didn't", "doesn", "doesn't", "hadn", "hadn't", "hasn", "hasn't", "haven", "haven't", "isn", "isn't", "mighn", "mightn't", "mustn", "mustn't", "needn", "needn't", "shan", "shan't", "shouldn", "shouldn't", "wasn", "wasn't", "weren","weren't", "won'", "won't", "wouldn", "wouldn't", "if" ]
-
 try:
-    nltk.data.find("corpora/stopwords")
+    nltk.data.find("corpora/stopwords.zip")
+    nltk.data.find("corpora/wordnet.zip")
+    nltk.data.find("corpora/omw-1.4.zip")
 except LookupError:
     nltk.download("stopwords")
+    nltk.download("wordnet")
+    nltk.download("omw-1.4")
+
+TO_KEEP_STOPWORDS = [ "this", "that'll", "these", "having", "does", "doing", "until", "while", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "few", "both", "more", "most", "other", "some", "than", "too", "very", "can", "will", "should", "should've", "now", "ain", "aren", "aren't", "could", "couldn", "couldn't", "didn", "didn't", "doesn", "doesn't", "hadn", "hadn't", "hasn", "hasn't", "haven", "haven't", "isn", "isn't", "mighn", "mightn't", "mustn", "mustn't", "needn", "needn't", "shan", "shan't", "shouldn", "shouldn't", "wasn", "wasn't", "weren","weren't", "won'", "won't", "wouldn", "wouldn't", "if" ]
+
 nltkstops = set(nltk.corpus.stopwords.words("english"))
 stops = [w for w in nltkstops if not w in TO_KEEP_STOPWORDS]
+
+lem = nltk.stem.wordnet.WordNetLemmatizer()
 
 def standardize(text):
     # convert to lower case
@@ -34,6 +41,35 @@ def standardize(text):
     text = tensorflow.strings.regex_replace(text, " +", " ")
     # remove leading and trailing white spaces
     text = tensorflow.strings.strip(text)
-    # TODO: remove only one word left?
     return text
+
+def clean(text):
+    # convert to lower case
+    text = text.lower()
+    # remove carriages and tabs
+    text = re.sub("(\n|\r|\t)", " ", text)
+    text = text.strip()
+    # remove links
+    text = re.sub("http\S+", "", text)
+    # remove hashtags, usernames and html entities
+    text = re.sub("(#|@|&|\$)\S+", "", text)
+    # remove punctuation
+    text = re.sub("[%s]" % re.escape(string.punctuation), " ", text)
+    # remove some stopwords
+    text = re.sub("\\b(" + "|".join(stops) + ")\\b\s*", "", text)
+    # keep only letters
+    text = re.sub("[^a-zA-Z]", " ", text)
+    # keep only words with more than 2 characters
+    text = re.sub("\\b\S\S?\\b", "", text)
+    # remove excess white spaces
+    text = re.sub(" +", " ", text)
+    # remove leading and trailing white spaces
+    text = text.strip()
+    # keep only sentences with four words minimum
+    if len(text.split()) > 4:
+        # lemmatize each word
+        text = "".join( [lem.lemmatize(w) for w in text] )
+        return text
+    else:
+        return None
 
