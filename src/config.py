b17c9531d22a64bb3a984921515c50c54fe26f2c
Ricardo Marliere <rbmarliere@gmail.com>
2022-07-29 20:17:46 -0300

    refactor + cleanup

--- src/config.py
+++ src/config.py
@@ -1,11 +1,13 @@
 import json
 import logging
+import os
 import sys
 
 class Config():
     def __init__(self):
         try:
-            self.config = json.load( open(".env") )
+            path = os.path.dirname(os.path.abspath(__file__))
+            self.config = json.load( open(path + "/../etc/.env") )
         except:
             logging.error(".env file not found! e.g. `ln -s .env.dev .env`")
             sys.exit(1)
