1c26a2272f5426f54cfe68ea7e1599c0fce6223c
Ricardo Marliere <rbmarliere@gmail.com>
2022-08-11 22:58:37 -0300

    db: use peewee as orm, refactor replay

--- src/db.py
+++ src/db.py
@@ -1,116 +1,86 @@
-import logging
 import numpy
-import pandas
-import psycopg2
-import src as hypnox
-import sys
+import peewee
 import tensorflow
 import transformers
 
+import src as hypnox
 
-def init():
-    # get config object
-    config = hypnox.config.Config()
-
-    # connect to database
-    try:
-        db = psycopg2.connect(host=config.config["DB_HOST"],
-                              database=config.config["DB_DATABASE"],
-                              user=config.config["DB_USER"],
-                              password=config.config["DB_PASSWORD"])
-        cursor = db.cursor()
-
-        return db, cursor
-
-    except psycopg2.errors.OperationalError:
-        logging.error("could not complete database connection")
-        sys.exit(1)
+connection = peewee.PostgresqlDatabase(
+    hypnox.config.config["DB_DATABASE"], **{
+        "host": hypnox.config.config["DB_HOST"],
+        "user": hypnox.config.config["DB_USER"],
+        "password": hypnox.config.config["DB_PASSWORD"]
+    })
 
 
-def close(db, c):
-    db.commit()
-    c.close()
-    db.close()
+class BaseModel(peewee.Model):
+
+    class Meta:
+        database = connection
 
 
-def insert(tweet):
-    db, c = init()
-    sql = "INSERT INTO stream_user(time,tweet) VALUES(%s, %s)"
-    c.execute(sql, (tweet["time"], tweet["tweet"]))
-    close(db, c)
+class Price(BaseModel):
+    time = peewee.DateTimeField(unique=True)
+    open = peewee.DecimalField(decimal_places=2, auto_round=True)
+    high = peewee.DecimalField(decimal_places=2, auto_round=True)
+    low = peewee.DecimalField(decimal_places=2, auto_round=True)
+    close = peewee.DecimalField(decimal_places=2, auto_round=True)
+    volume = peewee.DecimalField(decimal_places=2, auto_round=True)
+
+    class Meta:
+        table_name = "btcusdt_4h"
+        primary_key = False
+
+
+class Tweet(BaseModel):
+    time = peewee.DateTimeField()
+    text = peewee.TextField(null=False)
+    model_i = peewee.TextField(default="")
+    intensity = peewee.DecimalField(default=0)
+    model_p = peewee.TextField(default="")
+    polarity = peewee.DecimalField(default=0)
+
+    class Meta:
+        table_name = "tweets"
 
 
 def replay(args):
-    # load model config
     model_config = hypnox.config.ModelConfig(args.model)
     model_config.check_model()
 
-    # load transformer
     bert = transformers.TFAutoModel.from_pretrained(
         model_config.yaml["bert"],
         num_labels=model_config.yaml["num_labels"],
         from_pt=True)
     tokenizer = transformers.AutoTokenizer.from_pretrained(
         model_config.yaml["bert"])
-    labels = {0: 0, 2: -1, 1: 1}
 
-    # load model
     model = tensorflow.keras.models.load_model(
         model_config.model_path, custom_objects={"TFBertModel": bert})
 
-    # start database connection
-    db, c = init()
-
-    # check if target table exists
-    try:
-        query = "SELECT * FROM \"%s\" LIMIT 1" % args.table
-        c.execute(query)
-    except psycopg2.errors.UndefinedTable:
-        logging.error("table \"%s\" doesn't exist" % args.table)
-        sys.exit(1)
-
-    # check which column to use
     if model_config.yaml["class"] == "polarity":
-        modelcol = "model_p"
-        target = "polarity"
+        model_column = getattr(hypnox.db.Tweet, "model_p")
+        score_column = getattr(hypnox.db.Tweet, "polarity")
     elif model_config.yaml["class"] == "intensity":
-        modelcol = "model_i"
-        target = "intensity"
-    else:
-        logging.error(
-            "could not parse model config file (model class is missing)")
-        sys.exit(1)
+        model_column = getattr(hypnox.db.Tweet, "model_i")
+        score_column = getattr(hypnox.db.Tweet, "intensity")
 
-    # check which rows to update
+    query = hypnox.db.Tweet.select()
     if args.update_only:
-        update_only = "WHERE %s IS NULL OR %s <> '%s'" % (modelcol, modelcol,
-                                                          args.model)
-    else:
-        update_only = ""
+        query = query.where((model_column != model_config.yaml["name"])
+                            | (model_column.is_null()))
 
-    # fetch rows to update
-    query = "SELECT * FROM \"%s\" %s" % (args.table, update_only)
-    c.execute(query)
-    rows = c.fetchall()
-    if not rows:
-        logging.error("no rows to update")
-        sys.exit(1)
-
-    # load data
-    ids = [row[0] for row in rows]
-    df = pandas.DataFrame(ids, columns=["id"])
-    df["tweet"] = [row[2] for row in rows]
-
-    # preprocess model input
-    df["clean_tweet"] = df["tweet"].apply(hypnox.text_utils.standardize)
-    df = df.dropna()
-
-    # compute predictions
-    inputs = tokenizer(df["clean_tweet"].values.tolist(),
+    tweets = []
+    for tweet in query:
+        cleaned_text = hypnox.text_utils.standardize(tweet.text)
+        if cleaned_text is not None:
+            tweets.append(tweet)
+    inputs = tokenizer([tweet.text for tweet in tweets],
                        truncation=True,
-                       padding='max_length',
+                       padding="max_length",
                        max_length=model_config.yaml["max_length"],
                        return_tensors="tf")
+
     prob = model.predict(
         {
             "input_ids": inputs["input_ids"],
@@ -118,32 +88,22 @@ def replay(args):
         },
         verbose=1)
 
-    # check model class
     if model_config.yaml["class"] == "polarity":
-        df["polarity"] = [
-            labels[numpy.argmax(x)] * x[numpy.argmax(x)] for x in prob
-        ]
-        df["polarity"] = df["polarity"].apply("{:.8f}".format).apply(
-            pandas.to_numeric)
-    else:
-        df["intensity"] = [x[1] for x in prob]
-        df["intensity"] = df["intensity"].apply("{:.8f}".format).apply(
-            pandas.to_numeric)
+        labels = {0: 0, 2: -1, 1: 1}
+        i = 0
+        for tweet in tweets:
+            tweet.model_p = model_config.yaml["name"]
+            max = numpy.argmax(prob[i]).item()
+            tweet.polarity = labels[max] * prob[i][max]
+            i += 1
+    elif model_config.yaml["class"] == "intensity":
+        i = 0
+        for tweet in tweets:
+            tweet.model_i = model_config.yaml["name"]
+            tweet.intensity = prob[i][1].item()
+            i += 1
 
-    # update database
-    try:
-        tuples = str(
-            list(
-                zip(df["id"].values, df[target].values,
-                    [args.model] * len(df))))[1:-1]
-        update = "UPDATE \"%s\" AS t SET %s = t2.%s, %s = t2.%s" \
-            % args.table, target, target, modelcol, modelcol
-        update += "FROM (values %s) AS t2(id,%s,%s) WHERE t2.id = t.id" \
-            % tuples, target, modelcol
-        c.execute(update)
-    except psycopg2.errors.OperationalError:
-        logging.error("could not complete database update")
-        sys.exit(1)
-
-    # close database connection
-    close(db, c)
+    with hypnox.db.connection.atomic():
+        hypnox.db.Tweet.bulk_update(tweets,
+                                    fields=[model_column, score_column],
+                                    batch_size=1024)
