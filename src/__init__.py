a05c10379b097b539c7d5c653f6ac27d5dee8ea7
Ricardo Marliere <rbmarliere@gmail.com>
2022-08-11 23:03:51 -0300

    downloader + backtest work in progress

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,5 @@
-from . import (
-    config,
-    db,
-    model,
-    stream,
-    text_utils,
-)
+from . import config, db, exchange, model, strategy, stream, text_utils
 
-__all__ = ["config", "db", "model", "stream", "text_utils"]
+__all__ = [
+    "config", "db", "exchange", "model", "stream", "strategy", "text_utils"
+]
