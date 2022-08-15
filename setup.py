e297125b59a1c41e38ae6de82ba5a10189d7b991
Ricardo Marliere <rbmarliere@gmail.com>
2022-08-15 11:44:34 -0300

    cleanup

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
 from setuptools import find_packages, setup
 
 setup(name="hypnox", version="0.1", packages=find_packages())
-
-# TODO: restart venv & split into requirements-dev.txt and requirements.txt
