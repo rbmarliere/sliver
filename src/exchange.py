d4a5298915fdda8a53429b79828c19a057086c5c
Ricardo Marliere <rbmarliere@gmail.com>
2022-08-15 11:05:20 -0300

    improve download function

--- src/exchange.py
+++ src/exchange.py
@@ -3,8 +3,12 @@ import logging
 import time
 
 import ccxt
+import pandas
+import peewee
 
 import src as hypnox
+
+# https://docs.ccxt.com/en/latest/manual.html?#exception-hierarchy
 
 exchange_id = "binance"
 exchange_class = getattr(ccxt, exchange_id)
@@ -17,37 +21,63 @@ exchange = exchange_class({
 def download(args):
     sleep_time = (exchange.rateLimit / 1000) + 1
     page_size = 500
-    page_start = int(datetime.datetime(2020, 6, 1).timestamp() * 1000)
+    page_start = int(datetime.datetime(2011, 6, 1).timestamp() * 1000)
+    timeframe = 4 * 60 * 60 * 1000  # 4h in millisecs
 
-    prices = []
+    try:
+        first_entry = int(hypnox.db.Price.select().order_by(
+            hypnox.db.Price.time.asc()).get().time.timestamp() * 1000)
+        last_entry = int(hypnox.db.Price.select().order_by(
+            hypnox.db.Price.time.desc()).get().time.timestamp() * 1000)
+    except peewee.DoesNotExist:
+        first_entry = 0
+        last_entry = 0
+        logging.info("no db entries found, downloading everything...")
+
+    stop_at_first = False
+    if first_entry > page_start:
+        stop_at_first = True
+    elif last_entry > page_start:
+        page_start = last_entry + timeframe
+
+    prices = pandas.DataFrame(columns=[0, 1, 2, 3, 4, 5])
     while True:
         try:
-            logging.info("downloading from " + str(page_start))
+            logging.info("downloading from " +
+                         str(datetime.datetime.fromtimestamp(page_start /
+                                                             1000)))
             page = exchange.fetch_ohlcv("BTC/USDT",
                                         since=page_start,
                                         timeframe="4h",
                                         limit=page_size)
-            prices += list(
-                map(
-                    lambda x: [
-                        datetime.datetime.utcfromtimestamp(x[0] / 1000), x[1],
-                        x[2], x[3], x[4], x[5]
-                    ], page))
+            prices = pandas.concat(
+                [prices, pandas.DataFrame.from_records(page)])
+
+            if stop_at_first and page[-1][0] > first_entry:
+                for p in page:
+                    if p[0] >= first_entry:
+                        prices = prices[prices[0] < p[0]]
+                        break
+                page_start = last_entry + timeframe
+                stop_at_first = False
+                continue
+
             if len(page) < page_size:
                 break
-            page_start = page[-1][0] + 4 * 60 * 60 * 1000
+
+            page_start = page[-1][0] + timeframe
         except ccxt.RateLimitExceeded:
             logging.warning("rate limit exceeded, sleeping for " +
                             str(sleep_time) + " seconds")
             time.sleep(sleep_time)
             continue
-        # TODO: better error handling
-        # https://docs.ccxt.com/en/latest/manual.html?#exception-hierarchy
+
+    prices[0] = prices[0].apply(
+        lambda x: datetime.datetime.fromtimestamp(x / 1000))
 
     with hypnox.db.connection.atomic():
-        hypnox.db.Price.create_table()
         hypnox.db.Price.insert_many(
-            prices,
+            prices.values.tolist(),
             fields=[
                 hypnox.db.Price.time, hypnox.db.Price.open,
                 hypnox.db.Price.high, hypnox.db.Price.low,
