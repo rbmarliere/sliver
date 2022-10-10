b7185ca09c8a1d1469a12aefbc134318dc553407
Ricardo Marliere <rbmarliere@gmail.com>
2022-10-10 16:17:42 -0300

    fixes

--- hypnox/telegram.py
+++ hypnox/telegram.py
@@ -5,8 +5,8 @@ import hypnox
 
 def notify(message):
     try:
-        bot = telegram.Bot(hypnox.config["TELEGRAM_KEY"])
+        bot = telegram.Bot(hypnox.config["HYPNOX_TELEGRAM_KEY"])
         bot.send_message(text=message,
-                         chat_id=hypnox.config["TELEGRAM_CHANNEL"])
+                         chat_id=hypnox.config["HYPNOX_TELEGRAM_CHANNEL"])
     except KeyError:
         pass
