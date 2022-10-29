b0bc2c6bf41fae41e87f865d927ac55aab478368
Ricardo Marliere <rbmarliere@gmail.com>
2022-10-29 11:09:02 -0300

    web: auth guard, login error handling, route navig

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from './auth.guard';
 import { LoginComponent } from './login/login.component';
+import { PositionComponent } from './position/position.component';
 
 const routes: Routes = [
       { path: 'login', component: LoginComponent },
+      { path: '', component: PositionComponent, canActivate: [AuthGuard] },
+      { path: 'positions', component: PositionComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
