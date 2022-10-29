e97fb734d57d5f7d8ab6eab020abf7cc566934c1
Ricardo Marliere <rbmarliere@gmail.com>
2022-10-28 23:00:31 -0300

    web: login page

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ web/src/app/user.ts
@@ -0,0 +1,6 @@
+export interface User {
+  email: string;
+  password: string;
+  access_key: string;
+  expires_at: number;
+}
