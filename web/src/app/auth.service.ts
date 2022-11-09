707df50667b3f92cddbf8443ea7062c225b170c5
Ricardo Marliere <rbmarliere@gmail.com>
2022-11-09 16:23:37 -0300

    web: better login and auth guards

--- web/src/app/auth.service.ts
+++ web/src/app/auth.service.ts
@@ -2,6 +2,9 @@ import * as moment from 'moment';
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+import { BehaviorSubject } from 'rxjs';
 
 import { User } from './user';
 
@@ -10,28 +13,49 @@ import { User } from './user';
 })
 export class AuthService {
 
+  authenticated = new BehaviorSubject<boolean>(false);
+
   private getExpiration() {
-    const expiration = localStorage.getItem('expires_at')
+    const expiration = localStorage.getItem('expires_at');
     if (expiration === null) {
-      return null
+      return null;
     }
     const expiresAt = JSON.parse(expiration);
-    return moment(expiresAt)
+    return moment(expiresAt);
   }
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) { }
 
   login(email: string, password: string) {
     return this.http.post<User>('/v1/login', { email, password });
   }
 
   logout() {
-    localStorage.removeItem('access_key')
-    localStorage.removeItem('expires_at')
+    localStorage.removeItem('access_key');
+    localStorage.removeItem('expires_at');
+    this.authenticated.next(false);
+    this.router.navigateByUrl('/login');
+    location.reload();
+  }
+
+  setSession(authResult: User) {
+    const expiresAt = moment(authResult.expires_at, 'X');
+    localStorage.setItem('access_key', authResult.access_key);
+    localStorage.setItem('expires_at', JSON.stringify(expiresAt.valueOf()));
+    this.authenticated.next(true)
+    this.router.navigate(['positions']);
   }
 
   isAuthenticated() {
-    return moment().isBefore(this.getExpiration())
+    if (moment().isBefore(this.getExpiration())) {
+      this.authenticated.next(true);
+    } else {
+      this.authenticated.next(false);
+    }
+    return this.authenticated.asObservable();
   }
 
 }
