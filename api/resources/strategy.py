ddf9568b48b35b1807a7c4b259fc27c913b4b51d
Ricardo Marliere <rbmarliere@gmail.com>
2022-11-13 11:36:07 -0300

    api: subscribe to a strategy -> POST /strategies

--- api/resources/strategy.py
+++ api/resources/strategy.py
@@ -1,13 +1,13 @@
 from flask_jwt_extended import get_jwt_identity, jwt_required
-from flask_restful import Resource, fields, marshal_with
+from flask_restful import Resource, fields, marshal_with, reqparse
 
+import api
 import core
 
 fields = {
     "id": fields.Integer,
     "subscribed": fields.Boolean,
     "market_id": fields.Integer,
-    "active": fields.Boolean,
     "description": fields.String,
     "mode": fields.String,
     "timeframe": fields.String,
@@ -24,6 +24,10 @@ fields = {
     "tweet_filter": fields.String
 }
 
+argp = reqparse.RequestParser()
+argp.add_argument("strategy_id", type=int, required=True)
+argp.add_argument("subscribed", type=bool, required=True)
+
 
 class Strategy(Resource):
     @marshal_with(fields)
@@ -32,14 +36,41 @@ class Strategy(Resource):
         uid = int(get_jwt_identity())
         user = core.db.User.get_by_id(uid)
 
-        user_strats = [s for s in user.get_strategies()]
+        user_strats = [s.strategy for s in user.userstrategy_set.where(
+            core.db.UserStrategy.active)]
         strategies = [s for s in core.db.get_active_strategies()]
 
         for st in strategies:
             st.subscribed = False
-
-        for st, u_st in zip(strategies, user_strats):
-            if st == u_st:
+            if st in user_strats:
                 st.subscribed = True
 
         return strategies
+
+    @marshal_with(fields)
+    @jwt_required()
+    def post(self):
+        args = argp.parse_args()
+
+        try:
+            strategy = core.db.Strategy.get_by_id(args.strategy_id)
+        except core.db.Strategy.DoesNotExist:
+            raise api.errors.StrategyDoesNotExist
+
+        uid = int(get_jwt_identity())
+        user = core.db.User.get_by_id(uid)
+
+        user_strat_exists = False
+        for u_st in user.userstrategy_set:
+            if u_st.strategy == strategy:
+                user_strat_exists = True
+                u_st.active = args.subscribed
+                u_st.save()
+
+        if not user_strat_exists:
+            core.db.UserStrategy(
+                user=user, strategy=strategy, active=args.subscribed).save()
+
+        strategy.subscribed = args.subscribed
+
+        return strategy
