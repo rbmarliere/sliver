3209b3cadb153d18c5406d69f4650f2d15f4bb5b
Ricardo Marliere <rbmarliere@gmail.com>
2022-11-13 09:17:59 -0300

    api: new /prices route

--- api/resources/__init__.py
+++ api/resources/__init__.py
@@ -1,4 +1,4 @@
-from . import (credential, exchange, inventory, order, position,
+from . import (credential, exchange, inventory, order, position, price,
                strategy, user)
 
 Credential = credential.Credential
@@ -6,5 +6,6 @@ Exchange = exchange.Exchange
 Inventory = inventory.Inventory
 Order = order.Order
 Position = position.Position
+Price = price.Price
 Strategy = strategy.Strategy
 User = user.User
