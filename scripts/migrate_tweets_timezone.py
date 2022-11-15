e8b9693409dfe4f10a984cb3a81b1e8beb7ee8b0
Ricardo Marliere <rbmarliere@gmail.com>
2022-11-15 20:37:56 -0300

    scripts: migrate_tweets_timezone

--- scripts/migrate_tweets_timezone.py
+++ scripts/migrate_tweets_timezone.py
@@ -10,17 +10,10 @@ if __name__ == "__main__":
     with core.db.connection.atomic():
         tweets = [t for t in core.db.Tweet.select()]
         for tweet in tqdm(tweets):
-            new_time = datetime.utcfromtimestamp(tweet.time.timestamp())
-            if new_time != tweet.time:
-                print("old time: " + str(tweet.time))
-                print("new time for " + str(tweet.id) + ": " + str(new_time))
-                tweet.time = new_time
+            tweet.time = datetime.utcfromtimestamp(tweet.time.timestamp())
             tweet.model_i = None
             tweet.intensity = None
             tweet.model_p = None
             tweet.polarity = None
             tweet.save()
 
-        answer = input("Task completed. Continue?")
-        if answer.lower() in ["n", "no"]:
-            raise Exception
