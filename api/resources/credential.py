b59fc1199bd05cfa5aaef0fb9c3b8c34d34cc769
Ricardo Marliere <rbmarliere@gmail.com>
2022-11-22 09:58:58 -0300

    api: get /credentials return all exchanges

--- api/resources/credential.py
+++ api/resources/credential.py
@@ -5,6 +5,7 @@ import api.errors
 import core
 
 fields = {
+    "exchange_id": fields.Integer,
     "api_key": fields.String,
 }
 
@@ -14,20 +15,28 @@ argp.add_argument("api_key", type=str)
 argp.add_argument("api_secret", type=str)
 
 
-class Credential(Resource):
+class Credentials(Resource):
     @marshal_with(fields)
     @jwt_required()
-    def get(self, exchange_id):
+    def get(self):
         uid = int(get_jwt_identity())
         user = core.db.User.get_by_id(uid)
 
-        try:
-            credential = user.get_credential_by_exchange(exchange_id)
-        except core.db.Credential.DoesNotExist:
-            raise api.errors.CredentialDoesNotExist
+        credentials = []
+        for e in core.db.Exchange.select():
+            cred = user.credential_set.where(
+                core.db.Credential.exchange == e).get_or_none()
+            if cred is None:
+                cred = {
+                    "exchange_id": e.id,
+                    "api_key": ''
+                }
+            credentials.append(cred)
 
-        return credential
+        return credentials
 
+
+class Credential(Resource):
     @marshal_with(fields)
     @jwt_required()
     def post(self, exchange_id):
