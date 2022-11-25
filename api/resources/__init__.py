c2d1d5092e6e566184c654fa0c7951c776d9b49f
Ricardo Marliere <rbmarliere@gmail.com>
2022-11-25 15:30:46 -0300

    api: remove exchange resource

--- api/resources/__init__.py
+++ api/resources/__init__.py
@@ -1,8 +1,7 @@
-from . import (credential, exchange, inventory, order, position, price,
+from . import (credential, inventory, order, position, price,
                strategy, user)
 
 Credential = credential.Credential
-Exchange = exchange.Exchange
 Inventory = inventory.Inventory
 Order = order.Order
 Position = position.Position
--- api/routes.py
+++ api/routes.py
@@ -8,7 +8,6 @@ from .auth import Auth
 def init(api: flask_restful.Api):
     api.add_resource(Auth, "/login")
     api.add_resource(resources.Credential, "/credentials")
-    api.add_resource(resources.Exchange, "/exchanges")
     api.add_resource(resources.Inventory, "/inventory")
     api.add_resource(resources.Order, "/orders/<position_id>")
     api.add_resource(resources.Position, "/positions")
