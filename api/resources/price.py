420a086003cbc927adaefc5c58c7296ff34018d4
Ricardo Marliere <rbmarliere@gmail.com>
2022-11-26 10:44:48 -0300

    api: price route returns backtest output

--- api/resources/price.py
+++ api/resources/price.py
@@ -1,5 +1,3 @@
-import pandas
-import peewee
 from flask_jwt_extended import jwt_required
 from flask_restful import Resource, fields, marshal_with
 
@@ -18,6 +16,7 @@ fields = {
     "p_score": fields.List(fields.Float),
     "buys": fields.List(fields.Float),
     "sells": fields.List(fields.Float),
+    "backtest_log": fields.String
 }
 
 
@@ -30,34 +29,4 @@ class Price(Resource):
         except core.db.Strategy.DoesNotExist:
             raise api.errors.StrategyDoesNotExist
 
-        q = strategy\
-            .get_prices() \
-            .join(core.db.Indicator, peewee.JOIN.LEFT_OUTER) \
-            .select(core.db.Price, core.db.Indicator) \
-            .order_by(core.db.Price.id)
-
-        prices = pandas.DataFrame(q.dicts())
-
-        if prices.empty:
-            return
-
-        prices.time = prices.time.dt.strftime("%Y-%m-%d %H:%M")
-        prices.open = prices.open.apply(strategy.market.quote.format)
-        prices.high = prices.high.apply(strategy.market.quote.format)
-        prices.low = prices.low.apply(strategy.market.quote.format)
-        prices.close = prices.close.apply(strategy.market.quote.format)
-        prices.volume = prices.volume.apply(strategy.market.base.format)
-
-        prices["buys"] = (
-            (1 + .003) *
-            prices.close
-            .where(prices.signal == "buy"))
-        prices.buys = prices.buys.replace({float("nan"): None})
-
-        prices["sells"] = (
-            (1 - .003) *
-            prices.close
-            .where(prices.signal == "sell"))
-        prices.sells = prices.sells.replace({float("nan"): None})
-
-        return prices.to_dict("list")
+        return core.strategy.backtest(strategy)
