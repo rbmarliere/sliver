b51690a325466eff6d83bd0ca4ba4d3a1143be2c
Ricardo Marliere <rbmarliere@gmail.com>
2022-11-30 14:32:04 -0300

    scripts: format create_user

--- scripts/create_user.py
+++ scripts/create_user.py
@@ -7,12 +7,14 @@ import flask_bcrypt
 
 import core
 
-argp = argparse.ArgumentParser()
-argp.add_argument("--email", required=True)
-argp.add_argument("--name", required=True)
-args = argp.parse_args()
+if __name__ == "__main__":
+    argp = argparse.ArgumentParser()
+    argp.add_argument("--email",
+                      "-u",
+                      required=True)
+    args = argp.parse_args()
 
-passwd = getpass.getpass("enter user password: ")
-passwd = flask_bcrypt.generate_password_hash(passwd)
+    passwd = getpass.getpass("enter user password: ")
+    passwd = flask_bcrypt.generate_password_hash(passwd)
 
-core.db.User.create(email=args.email, password=passwd, name=args.name)
+    core.db.User.create(email=args.email, password=passwd)
