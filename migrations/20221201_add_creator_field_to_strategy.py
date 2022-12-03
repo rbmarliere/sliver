43c5998da22b4e463ab049792db6d8eeb0e172db
Ricardo Marliere <rbmarliere@gmail.com>
2022-12-03 10:25:10 -0300

    migrations: no need for backtest_log in strategy

--- migrations/20221201_add_creator_field_to_strategy.py
+++ migrations/20221201_add_creator_field_to_strategy.py
@@ -4,12 +4,9 @@ from playhouse.migrate import PostgresqlMigrator, migrate
 import core
 
 user = peewee.ForeignKeyField(core.db.User, field=core.db.User.id, default=1)
-backtest_log = peewee.TextField(null=True)
 
 migrator = PostgresqlMigrator(core.db.connection)
 
 migrate(
     migrator.add_column(core.db.Strategy._meta.table_name, "user_id", user),
-    migrator.add_column(core.db.Strategy._meta.table_name,
-                        "backtest_log", backtest_log),
 )
