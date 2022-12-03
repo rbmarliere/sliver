b593813264d77a7d9d76b10ee99b8c641da3002e
Ricardo Marliere <rbmarliere@gmail.com>
2022-12-03 18:54:52 -0300

    scripts: insert_exchange updates if row exists

--- scripts/insert_exchange.py
+++ scripts/insert_exchange.py
@@ -16,17 +16,27 @@ if __name__ == "__main__":
     args = argp.parse_args()
 
     try:
-        exchange = getattr(ccxt, args.exchange_name)
+        exchange = getattr(ccxt, args.exchange_name)({})
     except AttributeError:
         print("exchange not supported by ccxt")
         sys.exit(1)
 
-    if core.db.Exchange.get_or_none(name=args.exchange_name):
-        print("exchange already in database")
-        sys.exit(1)
+    l1 = set([i for i in exchange.timeframes.keys()])
+    l2 = set(core.utils.get_timeframes())
 
-    core.db.Exchange.create(
-        name=args.exchange_name,
-        rate_limit=exchange.rateLimit,
-        precision_mode=exchange.precisionMode,
-        padding_mode=exchange.paddingMode)
+    obj = {
+        "name": args.exchange_name,
+        "rate_limit": exchange.rateLimit,
+        "precision_mode": exchange.precisionMode,
+        "padding_mode": exchange.paddingMode,
+        "timeframes": str(list(l1 & l2))
+    }
+
+    exchange = core.db.Exchange(**obj)
+
+    db_exch = core.db.Exchange.get_or_none(name=args.exchange_name)
+
+    if db_exch:
+        exchange.id = db_exch.id
+
+    exchange.save()
