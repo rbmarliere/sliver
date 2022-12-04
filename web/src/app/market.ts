a7c4c88e24cbd337528e9b0c56363fb0f1dc133e
Ricardo Marliere <rbmarliere@gmail.com>
2022-12-04 16:02:05 -0300

    web: fix interfaces and add new ones

---
+++ web/src/app/market.ts
@@ -0,0 +1,12 @@
+export interface Market {
+  symbol: string,
+  exchange_name: string,
+  id: number,
+  base_id: number,
+  quote_id: number,
+  amount_precision: number,
+  price_precision: number,
+  amount_min: number,
+  cost_min: number,
+  price_min: number
+}
