22f678f91991293fc501f64d70377779f713a25b
Ricardo Marliere <rbmarliere@gmail.com>
2022-12-04 16:02:05 -0300

    web: fix interfaces and add new ones

---
+++ web/src/app/asset.ts
@@ -0,0 +1,5 @@
+export interface Asset {
+  id: number,
+  ticker: string,
+  precision: number
+}
---
+++ web/src/app/exchange.ts
@@ -0,0 +1,12 @@
+import { Asset } from "./asset";
+import { Market } from "./market";
+
+export interface Exchange {
+  name: string,
+  rate_limit: number,
+  precision_mode: number,
+  padding_mode: number,
+  timeframes: string[],
+  assets: Asset[],
+  markets: Market[]
+}
