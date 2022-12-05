5b9e6f7c94635fbf967387c2a5a149523a5ee31c
Ricardo Marliere <rbmarliere@gmail.com>
2022-12-04 21:00:23 -0300

    core: add active field to credential

---
+++ migrations/20221204_add_active_to_credential.py
@@ -0,0 +1,14 @@
+import peewee
+from playhouse.migrate import PostgresqlMigrator, migrate
+
+import core
+
+active = peewee.BooleanField(default=False)
+
+migrator = PostgresqlMigrator(core.db.connection)
+
+migrate(
+    migrator.add_column(core.db.Credential._meta.table_name,
+                        "active",
+                        active)
+)
