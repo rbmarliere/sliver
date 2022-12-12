6596db3a3b4a42e98d21dc9d8a533d0f0c3122d5
Ricardo Marliere <rbmarliere@gmail.com>
2022-12-12 09:39:47 -0300

    web: strategy-detail comp. uses new api route

--- web/src/app/strategy.service.ts
+++ web/src/app/strategy.service.ts
@@ -9,7 +9,8 @@ import { Strategy } from './strategy';
 })
 export class StrategyService {
 
-  url = 'v1/strategies';
+  strategies_url = 'v1/strategies';
+  strategy_url = 'v1/strategy';
 
   constructor(private http: HttpClient) { }
 
@@ -23,16 +24,27 @@ export class StrategyService {
 
   getStrategies(): Observable<Strategy[]> {
     return this.http
-      .get<Strategy[]>(this.url)
+      .get<Strategy[]>(this.strategies_url)
       .pipe(
         map(this.transformDate)
+      );
+  }
+
+  getStrategy(strategy_id: number): Observable<Strategy> {
+    return this.http
+      .get<Strategy>(this.strategy_url + "/" + strategy_id)
+      .pipe(
+        map((st) => {
+          st.next_refresh = st.next_refresh.slice(0, 16);
+          return st;
+        })
       );
   }
 
   updateSubscription(strategy: Strategy): Observable<Strategy> {
     const req = { id: strategy.id, subscribed: !strategy.subscribed };
     return this.http
-      .post<Strategy>(this.url, req)
+      .post<Strategy>(this.strategies_url, req)
       .pipe(
         map((st) => {
           st.next_refresh = st.next_refresh.slice(0, 16);
@@ -43,7 +55,7 @@ export class StrategyService {
 
   updateStrategy(strategy: Strategy): Observable<Strategy> {
     return this.http
-      .put<Strategy>(this.url, strategy)
+      .put<Strategy>(this.strategies_url, strategy)
       .pipe(
         map((st) => {
           st.next_refresh = st.next_refresh.slice(0, 16);
@@ -54,7 +66,7 @@ export class StrategyService {
 
   createStrategy(strategy: Strategy): Observable<Strategy> {
     return this.http
-      .post<Strategy>(this.url, strategy)
+      .post<Strategy>(this.strategies_url, strategy)
       .pipe(
         map((st) => {
           st.next_refresh = st.next_refresh.slice(0, 16);
