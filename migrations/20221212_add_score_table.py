0e2ab31f4850e8c4479c61f90ef3d34e72d11c05
Ricardo Marliere <rbmarliere@gmail.com>
2022-12-13 11:37:31 +0000

    migrations: fix score table

--- migrations/20221212_add_score_table.py
+++ migrations/20221212_add_score_table.py
@@ -4,6 +4,7 @@ from playhouse.migrate import PostgresqlMigrator, migrate
 
 import core
 
+core.db.Score.drop_table()
 core.db.Score.create_table()
 
 
@@ -18,28 +19,29 @@ class OldTweet(core.db.BaseModel):
     class Meta:
         table_name = "tweet"
 
+q = OldTweet.select().order_by(OldTweet.id)
 
-tweets = pandas.DataFrame(OldTweet.select().order_by(OldTweet.id).dicts())
-
-intensities = tweets[["id", "model_i", "intensity"]]
+i_filter = OldTweet.model_i.is_null(False)
+intensities = pandas.DataFrame(q.where(i_filter).dicts())
+intensities = intensities[["id", "model_i", "intensity"]]
 intensities = intensities.rename(
     columns={
         "id": "tweet_id",
         "model_i": "model",
         "intensity": "score"})
+core.db.Score.insert_many(intensities.to_dict("records")).execute()
 
-polarities = tweets[["id", "model_p", "polarity"]]
+p_filter = OldTweet.model_p.is_null(False)
+polarities = pandas.DataFrame(q.where(p_filter).dicts())
+polarities = polarities[["id", "model_p", "polarity"]]
 polarities = polarities.rename(
     columns={
         "id": "tweet_id",
         "model_p": "model",
         "polarity": "score"})
-
-core.db.Score.insert_many(intensities.to_dict("records")).execute()
 core.db.Score.insert_many(polarities.to_dict("records")).execute()
 
 migrator = PostgresqlMigrator(core.db.connection)
-
 migrate(
     migrator.drop_column(core.db.Tweet._meta.table_name, "model_i"),
     migrator.drop_column(core.db.Tweet._meta.table_name, "intensity"),
