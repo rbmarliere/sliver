d8301c363a5a446ffbb0b7a3998b4c528446dd12
Ricardo Marliere <rbmarliere@gmail.com>
2022-12-16 16:54:13 -0300

    web: backtest in client side

--- web/src/app/strategy-detail/strategy-detail.component.ts
+++ web/src/app/strategy-detail/strategy-detail.component.ts
@@ -65,10 +65,16 @@ export class StrategyDetailComponent implements OnInit {
       autorange: true,
       type: 'date'
     },
-    grid: {rows: 3, columns: 1},
-  }
+    grid: { rows: 3, columns: 1 },
+  };
+
+  plot_config = {
+    modeBarButtonsToRemove: ['select2d', 'lasso2d', 'zoomIn2d', 'zoomOut2d']
+  };
 
   plot_data: any;
+
+  backtest_log: any;
 
   loading: Boolean = true;
 
@@ -161,21 +167,21 @@ export class StrategyDetailComponent implements OnInit {
               type: 'candlestick',
               xaxis: 'x',
               yaxis: 'y'
-            },{
+            }, {
               name: 'i_score',
               x: res.prices.time,
               y: res.prices.i_score,
               type: 'line',
               xaxis: 'x',
               yaxis: 'y2'
-            },{
+            }, {
               name: 'p_score',
               x: res.prices.time,
               y: res.prices.p_score,
               type: 'line',
               xaxis: 'x',
               yaxis: 'y3'
-            },{
+            }, {
               name: 'buy signal',
               x: res.prices.time,
               y: res.prices.buys,
@@ -184,7 +190,7 @@ export class StrategyDetailComponent implements OnInit {
               marker: { color: 'green', size: 8 },
               xaxis: 'x',
               yaxis: 'y'
-            },{
+            }, {
               name: 'sell signal',
               x: res.prices.time,
               y: res.prices.sells,
@@ -195,6 +201,7 @@ export class StrategyDetailComponent implements OnInit {
               yaxis: 'y'
             }
           ];
+          this.zoom(0);
         },
         error: (err) => this.handleError(err)
       });
@@ -239,4 +246,124 @@ export class StrategyDetailComponent implements OnInit {
   formatLabel(value: number) {
     return Math.round(value * 100) + '%';
   }
+
+  zoom(event: any): void {
+    const start = event['xaxis.range[0]'];
+    const end = event['xaxis.range[1]'];
+    if (start) {
+      this.backtest(start, end);
+    } else {
+      this.backtest(
+        this.strategy.prices.time[0],
+        this.strategy.prices.time[this.strategy.prices.time.length - 1]);
+    }
+  }
+
+  backtest(_start: string, _end: string): void {
+    // filter data based on zoomed region
+    var start = new Date(_start);
+    var end = new Date(_end);
+    var times = this.strategy.prices.time;
+    var buys = this.strategy.prices.buys;
+    var sells = this.strategy.prices.sells;
+    var len = times.length;
+    var indexes = [];
+    for (var i = 0; i < len; i++) {
+      var element = this.strategy.prices.time[i];
+      if (new Date(element) >= start && new Date(element) <= end) {
+        indexes.push(i);
+      }
+    }
+
+    // get existing positions in filtered data
+    var positions = []
+    var curr = false;
+    var currPos = {
+      entry_price: 0,
+      entry_time: new Date(0),
+      exit_price: 0,
+      exit_time: new Date(0)
+    };
+    for (i = 0; i < indexes.length; i++) {
+      var idx = indexes[i];
+      if (buys[idx] > 0) {
+        if (!curr) {
+          curr = true;
+          currPos.entry_price = buys[idx];
+          currPos.entry_time = new Date(times[idx]);
+        }
+      } else if (sells[idx] > 0) {
+        if (curr) {
+          curr = false;
+          currPos.exit_price = sells[idx];
+          currPos.exit_time = new Date(times[idx]);
+          positions.push({
+            entry_price: currPos.entry_price,
+            entry_time: currPos.entry_time,
+            exit_price: currPos.exit_price,
+            exit_time: currPos.exit_time
+          });
+        }
+      }
+    }
+
+    if (positions.length == 0) {
+      this.backtest_log = "no positions opened";
+      return;
+    }
+
+    // compute metrics based on found positions
+    var init_balance = 10000;
+    var balance = 10000;
+    var avg_time = 0;
+    var avg_roi = 0;
+    for (i = 0; i < positions.length; i++) {
+      var pos = positions[i];
+
+      var pricedelta = pos.exit_price - pos.entry_price;
+      var timedelta = pos.exit_time.getTime() - pos.entry_time.getTime();
+
+      avg_time = (avg_time * i + timedelta) / (i + 1);
+
+      var new_balance = balance + pricedelta;
+      var roi = ((new_balance / balance) - 1) * 100;
+      avg_roi = (avg_roi * i + roi) / (i + 1);
+
+      balance = new_balance
+    }
+
+    var init_bh_amount = init_balance / this.strategy.prices.open[indexes[0]];
+    var exit_bh_value = init_bh_amount * this.strategy.prices.open[indexes[indexes.length - 1]];
+
+    var roi = ((balance / init_balance) - 1) * 100;
+    var roi_bh = ((exit_bh_value / init_balance) - 1) * 100;
+
+    this.backtest_log = `
+initial balance = ${init_balance.toFixed(2)}
+final balance = ${balance.toFixed(2)}
+pnl = ${(balance - init_balance).toFixed(2)}
+roi = ${roi.toFixed(2)}%
+total timedelta = ${this.msToString(end.getTime() - start.getTime())}
+number of trades = ${positions.length}
+average timedelta in position = ${this.msToString(avg_time)}
+average position roi = ${avg_roi.toFixed(2)}%
+buy and hold amount at first candle = ${init_bh_amount.toFixed(8)}
+buy and hold value at last candle = ${exit_bh_value.toFixed(2)}
+buy and hold roi = ${roi_bh.toFixed(2)}%
+`;
+
+    console.log(positions);
+  }
+
+  msToString(ms: number): string {
+    // https://stackoverflow.com/questions/29816872/how-can-i-convert-milliseconds-to-hhmmss-format-using-javascript
+    var seconds = ms / 1000;
+    const days = parseInt((seconds / 86400).toString());
+    seconds = seconds % 86400;
+    const hours = parseInt((seconds / 3600).toString());
+    seconds = seconds % 3600;
+    const minutes = parseInt((seconds / 60).toString());
+    return `${days}d ${hours}h ${minutes}m`
+  }
+
 }
