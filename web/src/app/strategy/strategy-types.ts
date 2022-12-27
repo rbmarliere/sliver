1ba41f082032d996586d1d2814c5f0f436f6a854
Ricardo Marliere <rbmarliere@gmail.com>
2022-12-27 18:40:49 -0300

    web: add getStrategyTypes to strategy component

---
+++ web/src/app/strategy/strategy-types.ts
@@ -0,0 +1,21 @@
+export interface StrategyType {
+  value: number;
+  name: string;
+}
+
+export function getStrategyTypes(): StrategyType[] {
+  return [
+    {
+      value: 0,
+      name: 'MANUAL',
+    },
+    {
+      value: 1,
+      name: 'RANDOM',
+    },
+    {
+      value: 2,
+      name: 'HYPNOX',
+    }
+  ];
+}
