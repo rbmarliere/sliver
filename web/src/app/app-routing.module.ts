45279d14050939f1083da25642e22b34b6d30556
Ricardo Marliere <rbmarliere@gmail.com>
2022-12-27 14:51:59 -0300

    web: format code

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { AuthGuard } from './auth.guard';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginGuard } from './login.guard';
@@ -14,14 +13,30 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent, canActivate: [LoginGuard] },
   { path: '', redirectTo: '/positions', pathMatch: 'full' },
   { path: 'positions', component: PositionComponent, canActivate: [AuthGuard] },
-  { path: 'position/:position_id', component: OrderComponent, canActivate: [AuthGuard] },
-  { path: 'strategies', component: StrategyComponent, canActivate: [AuthGuard] },
-  { path: 'strategy/:strategy_id', component: StrategyDetailComponent, canActivate: [AuthGuard] },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  {
+    path: 'position/:position_id',
+    component: OrderComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'strategies',
+    component: StrategyComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'strategy/:strategy_id',
+    component: StrategyDetailComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [AuthGuard],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,40 +1,36 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor } from './auth.interceptor';
-
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatTableModule } from '@angular/material/table';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatGridListModule } from '@angular/material/grid-list';
-
-import { NavbarComponent } from './navbar/navbar.component';
-import { LoginComponent } from './login/login.component';
-import { PositionComponent } from './position/position.component';
-import { ErrorDialogComponent } from './error-dialog/error-dialog.component';
-import { StrategyComponent } from './strategy/strategy.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { OrderComponent } from './order/order.component';
-import { CredentialComponent } from './credential/credential.component';
-import { StrategyDetailComponent } from './strategy-detail/strategy-detail.component';
-
+import { MatSliderModule } from '@angular/material/slider';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PlotlyViaWindowModule } from 'angular-plotly.js';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthInterceptor } from './auth.interceptor';
+import { CredentialComponent } from './credential/credential.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ErrorDialogComponent } from './error-dialog/error-dialog.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { OrderComponent } from './order/order.component';
+import { PositionComponent } from './position/position.component';
+import { StrategyDetailComponent } from './strategy-detail/strategy-detail.component';
+import { StrategyComponent } from './strategy/strategy.component';
 
 @NgModule({
   declarations: [
@@ -76,9 +72,9 @@ import { PlotlyViaWindowModule } from 'angular-plotly.js';
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
-      multi: true
-    }
+      multi: true,
+    },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
