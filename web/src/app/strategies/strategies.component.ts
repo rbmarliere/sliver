437436bd942ab8f7e95d18fa4f1eaf03a7bc5162
Ricardo Marliere <rbmarliere@gmail.com>
2022-12-28 19:45:52 -0300

    web: add strattype name to strategies component table

--- web/src/app/strategies/strategies.component.ts
+++ web/src/app/strategies/strategies.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { StrategiesService } from '../strategies.service';
 import { BaseStrategy } from '../strategy';
 import { StrategyService } from '../strategy.service';
+import { getStrategyTypes } from '../strategy/strategy-types';
 
 @Component({
   selector: 'app-strategies',
@@ -12,6 +13,7 @@ export class StrategiesComponent implements OnInit {
   displayedColumns: string[] = [
     'id',
     'active',
+    'type',
     'description',
     'symbol',
     'exchange',
@@ -44,5 +46,16 @@ export class StrategiesComponent implements OnInit {
     this.strategyService.deleteStrategy(strategy_id).subscribe({
       next: () => location.reload(),
     });
+  }
+
+  getTypeName(type: number): string {
+    let name = '';
+    getStrategyTypes().forEach((strategy_type) => {
+      if (strategy_type.value === type) {
+        name = strategy_type.name;
+      }
+    });
+
+    return name;
   }
 }
