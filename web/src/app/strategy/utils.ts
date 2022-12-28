587e9554cea75df5d7b782ef35590f9a56d997a0
Ricardo Marliere <rbmarliere@gmail.com>
2022-12-28 15:07:40 -0300

    web: fix strat utils, median copy bug, etc

--- web/src/app/strategy/utils.ts
+++ web/src/app/strategy/utils.ts
@@ -1,5 +1,4 @@
 export function msToString(ms: number): string {
-  // https://stackoverflow.com/questions/29816872/how-can-i-convert-milliseconds-to-hhmmss-format-using-javascript
   var seconds = ms / 1000;
   const days = parseInt((seconds / 86400).toString());
   seconds = seconds % 86400;
@@ -9,10 +8,12 @@ export function msToString(ms: number): string {
   return `${days}d ${hours}h ${minutes}m`;
 }
 
-export function median(values: any): any {
-  // https://stackoverflow.com/questions/45309447/calculating-median-javascript
-  if (values.length === 0) return 0;
-  if (containsNaN(values)) return 0;
+export function median(_values: number[]): string {
+  let values = [..._values]; // copy first, since sort() is in-place
+
+  if (values.length === 0) {
+    return "0";
+  }
 
   values.sort(function(a: number, b: number) {
     return a - b;
@@ -20,26 +21,19 @@ export function median(values: any): any {
 
   var half = Math.floor(values.length / 2);
 
-  if (values.length % 2) return values[half];
+  if (values.length % 2) {
+    return values[half].toFixed(4);
+  };
 
-  return (values[half - 1] + values[half]) / 2.0;
+  return ((values[half - 1] + values[half]) / 2.0).toFixed(4);
 }
 
-export function mean(values: any): any {
-  if (values.length === 0) return 0;
-  if (containsNaN(values)) return 0;
-
-  const sum = values.reduce((a: any, b: any) => a + b, 0);
-  const avg = sum / values.length || 0;
-  return avg;
-}
-
-// function that checks if a list contains a NaN value
-export function containsNaN(values: any): boolean {
-  for (var i = 0; i < values.length; i++) {
-    if (isNaN(values[i])) {
-      return true;
-    }
+export function mean(values: number[]): string {
+  if (values.length === 0) {
+    return "0";
   }
-  return false;
+
+  const sum = values.reduce((a: number, b: number) => a + b, 0);
+  const avg = sum / values.length || 0;
+  return avg.toFixed(4);
 }
