fdc6de3f1591b6389bb832dc9184baaaa3842c42
Ricardo Marliere <rbmarliere@gmail.com>
2023-01-02 14:56:06 -0300

    strat: paginate hypnox replay query

--- strategies/hypnox/replay.py
+++ strategies/hypnox/replay.py
@@ -42,24 +42,32 @@ def replay(model, update_only=True, verbose=0):
     if update_only:
         query = query.where(strategies.hypnox.HypnoxScore.model.is_null())
 
-    tweets = pandas.DataFrame(query.dicts())
-
-    if tweets.empty:
+    count = query.count()
+    if count == 0:
         core.watchdog.info("{m}: no tweets to replay"
                            .format(m=model.config["name"]))
         return
-
     core.watchdog.info("{m}: replaying {c} tweets"
-                       .format(c=query.count(),
+                       .format(c=count,
                                m=model.config["name"]))
 
-    tweets.text = tweets.text.apply(core.utils.standardize)
-    tweets.text = tweets.text.str.slice(0, model.config["max_length"])
+    with core.db.connection.atomic():
+        page = 0
+        while True:
+            page_q = query.paginate(page, 1024)
+            page += 1
+            if page_q.count() == 0:
+                break
 
-    tweets = tweets.rename(columns={"id": "tweet_id"})
-    tweets["model"] = model.config["name"]
-    tweets["score"] = predict(model, tweets["text"].to_list(), verbose=verbose)
+            tweets = pandas.DataFrame(page_q.dicts())
+            tweets.text = tweets.text.apply(core.utils.standardize)
+            tweets.text = tweets.text.str.slice(0, model.config["max_length"])
+            tweets = tweets.rename(columns={"id": "tweet_id"})
+            tweets["model"] = model.config["name"]
+            tweets["score"] = predict(model,
+                                      tweets["text"].to_list(),
+                                      verbose=verbose)
 
-    scores = tweets[["tweet_id", "model", "score"]]
-    strategies.hypnox.HypnoxScore.insert_many(
-        scores.to_dict("records")).execute()
+            scores = tweets[["tweet_id", "model", "score"]]
+            strategies.hypnox.HypnoxScore.insert_many(
+                scores.to_dict("records")).execute()
