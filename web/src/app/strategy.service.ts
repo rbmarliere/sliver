a0a439a979c9993d5dff5ee2d9d9b6c188bcd181
Ricardo Marliere <rbmarliere@gmail.com>
2023-01-02 14:58:57 -0300

    web: POST /strategy hack

--- web/src/app/strategy.service.ts
+++ web/src/app/strategy.service.ts
@@ -39,6 +39,7 @@ export class StrategyService {
   }
 
   updateStrategy(strategy: Strategy): Observable<Strategy> {
+    strategy.prices = {} as any;
     return this.http.put<Strategy>(this.url + '/' + strategy.id, strategy).pipe(
       map((st) => {
         st.next_refresh = st.next_refresh.slice(0, 16);
