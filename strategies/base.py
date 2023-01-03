2f6be122d3cf31f63af4d9eb75b75ee1423358be
Ricardo Marliere <rbmarliere@gmail.com>
2023-01-03 16:47:34 -0300

    strat: format price data

--- strategies/base.py
+++ strategies/base.py
@@ -48,6 +48,11 @@ class BaseStrategy(core.db.BaseModel):
                         sells.append(idx)
                         curr_pos = False
             df.time = df.time.dt.strftime("%Y-%m-%d %H:%M")
+            df.open = df.open.apply(self.strategy.market.quote.format)
+            df.high = df.high.apply(self.strategy.market.quote.format)
+            df.low = df.low.apply(self.strategy.market.quote.format)
+            df.close = df.close.apply(self.strategy.market.quote.format)
+            df.volume = df.volume.apply(self.strategy.market.base.format)
             df["buys"] = df.open.where(df.index.isin(buys))
             df.buys = df.buys.replace({float("nan"): None})
             df["sells"] = df.open.where(df.index.isin(sells))
