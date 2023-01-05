daa791b7bf2639e5db6b5eb5f818d957b8a9cbd9
Ricardo Marliere <rbmarliere@gmail.com>
2023-01-05 13:08:46 -0300

    strat: format dd3 ma values

--- strategies/dd3/dd3.py
+++ strategies/dd3/dd3.py
@@ -26,7 +26,11 @@ class DD3Strategy(BaseStrategy):
             .join(DD3Indicator, peewee.JOIN.LEFT_OUTER)
 
     def get_indicators_df(self):
-        return super().get_indicators_df(self.get_indicators())
+        df = super().get_indicators_df(self.get_indicators())
+        df.ma1 = df.ma1.apply(self.strategy.market.quote.format)
+        df.ma2 = df.ma2.apply(self.strategy.market.quote.format)
+        df.ma3 = df.ma3.apply(self.strategy.market.quote.format)
+        return df
 
     def refresh(self):
         self.refresh_indicators()
