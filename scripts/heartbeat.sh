a2a3e85db21ca28d53742072011aab432880a083
Ricardo Marliere <rbmarliere@gmail.com>
2023-01-05 20:41:35 -0300

    heartbeat

--- scripts/heartbeat.sh
+++ scripts/heartbeat.sh
@@ -3,7 +3,7 @@
 IP=""
 TELEGRAM_BOT_ID=""
 TELEGRAM_CHAT_ID=""
-TELEGRAM_API="https://api.telegram.org/bot$TELEGRAM_BOT_ID/sendMessage?chat_id=$TELEGRAM_CHAT_ID&text=$(hostname):%20"
+TELEGRAM="https://api.telegram.org/bot$TELEGRAM_BOT_ID/sendMessage?chat_id=$TELEGRAM_CHAT_ID&text=$(hostname):%20"
 
 if [ -z "$IP" ]; then
     if [ -z "$1" ]; then
@@ -12,12 +12,12 @@ if [ -z "$IP" ]; then
     fi
 
     if ! pgrep -x "$1" > /dev/null; then
-        curl -s "$TELEGRAM_API$1"%20is%20down
+        curl -s "$TELEGRAM"service%20"$1"%20is%20down
     fi
 
     exit 0
 fi
 
 if ! ping -c 1 "$IP" > /dev/null 2>&1; then
-    curl -s "$TELEGRAM_API"server%20is%20down
+    curl -s "$TELEGRAM"server%20is%20down
 fi
