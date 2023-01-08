099444d870f23157f7c98d06d69c7a8ce65680e0
Ricardo Marliere <rbmarliere@gmail.com>
2023-01-08 10:34:36 -0300

    core: better inventory

--- core/inventory.py
+++ core/inventory.py
@@ -9,48 +9,58 @@
 # payoff ratio,
 # win days
 
+import ccxt
+
 import core
 
 
+def get_balance_by_asset(user: core.db.User, asset: core.db.Asset):
+    balance = {
+        "ticker": asset.ticker,
+        "free": 0,
+        "used": 0,
+        "total": 0,
+        "free_value": 0,
+        "used_value": 0,
+        "total_value": 0
+    }
+
+    for bal in user.get_balances_by_asset(asset):
+        if bal.total_value == 0:
+            continue
+
+        asset = bal.asset.asset
+
+        free = bal.asset.format(bal.free)
+        used = bal.asset.format(bal.used)
+        total = bal.asset.format(bal.total)
+        balance["free"] += free
+        balance["used"] += used
+        balance["total"] += total
+
+        free_val = bal.value_asset.format(bal.free_value)
+        used_val = bal.value_asset.format(bal.used_value)
+        total_val = bal.value_asset.format(bal.total_value)
+        balance["free_value"] += free_val
+        balance["used_value"] += used_val
+        balance["total_value"] += total_val
+
+    return balance
+
+
 def get_inventory(user: core.db.User):
-    inventory = {}
+    balances = []
     inv_free_val = inv_used_val = inv_total_val = 0
 
     for asset in core.db.Asset.select():
-        for bal in user.get_balances_by_asset(asset):
-            if bal.total_value == 0:
-                continue
+        balance = get_balance_by_asset(user, asset)
+        balances.append(balance)
+        inv_free_val += balance["free_value"]
+        inv_used_val += balance["used_value"]
+        inv_total_val += balance["total_value"]
 
-            asset = bal.asset.asset
-
-            if asset.ticker not in inventory:
-                inventory[asset.ticker] = {
-                    "free": 0,
-                    "used": 0,
-                    "total": 0,
-                    "free_value": 0,
-                    "used_value": 0,
-                    "total_value": 0
-                }
-
-            free = bal.asset.format(bal.free)
-            used = bal.asset.format(bal.used)
-            total = bal.asset.format(bal.total)
-            inventory[asset.ticker]["free"] += free
-            inventory[asset.ticker]["used"] += used
-            inventory[asset.ticker]["total"] += total
-
-            free_val = bal.value_asset.format(bal.free_value)
-            used_val = bal.value_asset.format(bal.used_value)
-            total_val = bal.value_asset.format(bal.total_value)
-            inventory[asset.ticker]["free_value"] += free_val
-            inventory[asset.ticker]["used_value"] += used_val
-            inventory[asset.ticker]["total_value"] += total_val
-
-            inv_free_val += free_val
-            inv_used_val += used_val
-            inv_total_val += total_val
-
+    inventory = {}
+    inventory["balances"] = balances
     inventory["free_value"] = inv_free_val
     inventory["used_value"] = inv_used_val
     inventory["total_value"] = inv_total_val
@@ -98,7 +108,8 @@ def get_target_cost(user_strat: core.db.UserStrategy):
                        .format(a=user_strat.strategy.market.quote.asset.ticker,
                                v=print(cash_in_exch)))
 
-    cash_liquid = transform(inventory["USDT"]["total"]
+    usdt_balance = get_balance_by_asset(user, core.db.Asset.get(ticker="USDT"))
+    cash_liquid = transform(usdt_balance["total"]
                             - inventory["positions_reserved"])
     available = min(cash_in_exch, cash_liquid) * (1 - user.cash_reserve)
     core.watchdog.info("available cash is {v}"
@@ -116,6 +127,75 @@ def get_target_cost(user_strat: core.db.UserStrategy):
 #   it will effectively deleverage by reducing open positions to fit new params
 
 
+def sync_balance(cred: core.db.Credential):
+    value_ticker = "USDT"
+    value_asset, new = core.db.Asset.get_or_create(ticker=value_ticker)
+    if new:
+        core.watchdog.info("saved asset {a}".format(a=value_asset.ticker))
+
+    try:
+        ex_bal = core.exchange.api.fetch_balance()
+    except ccxt.ExchangeError as e:
+        core.watchdog.error("exchange error", e)
+        cred.disable()
+        return
+
+    ex_val_asset, new = core.db.ExchangeAsset.get_or_create(
+        exchange=cred.exchange, asset=value_asset)
+    if new:
+        core.watchdog.info("saved asset " + ex_val_asset.asset.ticker +
+                           " to exchange")
+
+    # for each exchange asset, update internal user balance
+    for ticker in ex_bal["free"]:
+        free = ex_bal["free"][ticker]
+        used = ex_bal["used"][ticker]
+        total = ex_bal["total"][ticker]
+
+        if not total:
+            continue
+
+        asset, new = core.db.Asset.get_or_create(ticker=ticker.upper())
+        if new:
+            core.watchdog.info("saved new asset {a}"
+                               .format(a=asset.ticker))
+
+        ex_asset, new = core.db.ExchangeAsset.get_or_create(
+            exchange=cred.exchange, asset=asset)
+        if new:
+            core.watchdog.info("saved asset {a}"
+                               .format(a=ex_asset.asset.ticker))
+
+        u_bal, new = core.db.Balance.get_or_create(
+            user=cred.user, asset=ex_asset, value_asset=ex_val_asset)
+        if new:
+            core.watchdog.info("saved new balance {b}"
+                               .format(b=u_bal.id))
+
+        u_bal.free = ex_asset.transform(free) if free else 0
+        u_bal.used = ex_asset.transform(used) if used else 0
+        u_bal.total = ex_asset.transform(total) if total else 0
+
+        try:
+            p = core.exchange.api.fetch_ticker(asset.ticker + "/" +
+                                               value_asset.ticker)
+
+            free_value = p["last"] * free if free else 0
+            used_value = p["last"] * used if used else 0
+            total_value = p["last"] * total if total else 0
+
+            u_bal.free_value = ex_val_asset.transform(free_value)
+            u_bal.used_value = ex_val_asset.transform(used_value)
+            u_bal.total_value = ex_val_asset.transform(total_value)
+
+        except ccxt.BadSymbol:
+            u_bal.free_value = u_bal.free
+            u_bal.used_value = u_bal.used
+            u_bal.total_value = u_bal.total
+
+        u_bal.save()
+
+
 def sync_balances(user: core.db.User):
     # backup current api
     curr_cred = None
@@ -129,7 +209,7 @@ def sync_balances(user: core.db.User):
 
         core.exchange.set_api(cred=cred)
 
-        core.exchange.sync_balance(cred)
+        sync_balance(cred)
 
     # restore previous api
     if curr_cred:
