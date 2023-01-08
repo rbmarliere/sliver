8e7323ad6a8a9fbd5088c12e29e812412c58a74b
Ricardo Marliere <rbmarliere@gmail.com>
2023-01-08 10:34:49 -0300

    api: better inventory

--- api/resources/inventory.py
+++ api/resources/inventory.py
@@ -1,10 +1,31 @@
 from flask_jwt_extended import get_jwt_identity, jwt_required
-from flask_restful import Resource
+from flask_restful import Resource, fields, marshal_with
 
 import core
 
 
+balance_fields = {
+    "ticker": fields.String,
+    "free": fields.Float,
+    "used": fields.Float,
+    "total": fields.Float,
+    "free_value": fields.Float,
+    "used_value": fields.Float,
+    "total_value": fields.Float,
+}
+
+fields = {
+    "free_value": fields.Float,
+    "used_value": fields.Float,
+    "total_value": fields.Float,
+    "positions_reserved": fields.Float,
+    "positions_value": fields.Float,
+    "balances": fields.List(fields.Nested(balance_fields)),
+}
+
+
 class Inventory(Resource):
+    @marshal_with(fields)
     @jwt_required()
     def get(self):
         uid = int(get_jwt_identity())
