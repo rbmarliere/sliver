ecd7534771ef066e866a30304cdc3f0d7f665eba
Ricardo Marliere <rbmarliere@gmail.com>
2023-01-14 18:05:15 -0300

    api: new GET /indicators route

--- api/routes.py
+++ api/routes.py
@@ -1,7 +1,6 @@
 import flask_restful
 
 import api.resources as resources
-
 from .auth import Auth
 
 
@@ -9,9 +8,10 @@ def init(api: flask_restful.Api):
     api.add_resource(Auth, "/login")
     api.add_resource(resources.Credential, "/credentials")
     api.add_resource(resources.Exchange, "/exchanges")
+    api.add_resource(resources.Indicator, "/indicators/<strategy_id>")
     api.add_resource(resources.Inventory, "/inventory")
     api.add_resource(resources.Order, "/orders/<position_id>")
     api.add_resource(resources.Position, "/positions")
-    api.add_resource(resources.Strategy, "/strategy/<strategy_id>")
     api.add_resource(resources.Strategies, "/strategies")
+    api.add_resource(resources.Strategy, "/strategy/<strategy_id>")
     api.add_resource(resources.User, "/user")
