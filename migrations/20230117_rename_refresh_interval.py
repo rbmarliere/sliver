1aeadafc33d85aa6798aa4a04f12ab3903d1ef48
Ricardo Marliere <rbmarliere@gmail.com>
2023-01-17 15:02:16 -0300

    migrations: change strategy refresh fields

--- migrations/20230117_rename_refresh_interval.py
+++ migrations/20230117_rename_refresh_interval.py
@@ -7,6 +7,6 @@ migrator = PostgresqlMigrator(core.db.connection)
 
 migrate(
     migrator.rename_column(core.db.Strategy._meta.table_name,
-                           "min_roi",
-                           "stop_gain")
+                           "refresh_interval",
+                           "orders_interval")
 )
