903a5d6f29b98c6748bcd45ec9bc3c00a6429fe2
Ricardo Marliere <rbmarliere@gmail.com>
2023-01-17 15:04:16 -0300

    web: change strategy fields

--- web/src/app/strategy.service.ts
+++ web/src/app/strategy.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Strategy } from './strategy';
 
 @Injectable({
@@ -11,40 +11,17 @@ export class StrategyService {
 
   constructor(private http: HttpClient) { }
 
-  transformDate(strategies: Strategy[]): Strategy[] {
-    for (let strategy of strategies) {
-      strategy.next_refresh = strategy.next_refresh.slice(0, 16);
-    }
-
-    return strategies;
-  }
-
   getStrategy(strategy_id: number): Observable<Strategy> {
-    return this.http.get<Strategy>(this.url + '/' + strategy_id).pipe(
-      map((st) => {
-        st.next_refresh = st.next_refresh.slice(0, 16);
-        return st;
-      })
-    );
+    return this.http.get<Strategy>(this.url + '/' + strategy_id);
   }
 
   updateSubscription(strategy: Strategy): Observable<Strategy> {
     const req = { subscribe: true, subscribed: !strategy.subscribed };
-    return this.http.put<Strategy>(this.url + '/' + strategy.id, req).pipe(
-      map((st) => {
-        st.next_refresh = st.next_refresh.slice(0, 16);
-        return st;
-      })
-    );
+    return this.http.put<Strategy>(this.url + '/' + strategy.id, req);
   }
 
   updateStrategy(strategy: Strategy): Observable<Strategy> {
-    return this.http.put<Strategy>(this.url + '/' + strategy.id, strategy).pipe(
-      map((st) => {
-        st.next_refresh = st.next_refresh.slice(0, 16);
-        return st;
-      })
-    );
+    return this.http.put<Strategy>(this.url + '/' + strategy.id, strategy);
   }
 
   deleteStrategy(strategy_id: number): Observable<any> {
