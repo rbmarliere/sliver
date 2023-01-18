cc7e645808b92e8ed02cf3e7f0e59a66b488db7e
Ricardo Marliere <rbmarliere@gmail.com>
2023-01-18 19:16:49 -0300

    migrations: trailing stops

---
+++ migrations/20230118_last_high_low.py
@@ -0,0 +1,16 @@
+from playhouse.migrate import PostgresqlMigrator, migrate
+
+import core
+import peewee
+
+migrator = PostgresqlMigrator(core.db.connection)
+
+
+migrate(
+    migrator.rename_column(core.db.Position._meta.table_name,
+                           "last_price",
+                           "last_high"),
+    migrator.add_column(core.db.Position._meta.table_name,
+                        "last_low",
+                        peewee.BigIntegerField(default=0))
+)
