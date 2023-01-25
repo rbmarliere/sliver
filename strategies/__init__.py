77e376cd3e3e87a8c7bd03450a49704f2dc3803b
Ricardo Marliere <rbmarliere@gmail.com>
2023-01-25 16:43:16 -0300

    strat: new MIXER type

--- strategies/__init__.py
+++ strategies/__init__.py
@@ -1,8 +1,10 @@
 import enum
 
+import core
 from .dd3 import DD3Strategy
 from .hypnox import HypnoxStrategy
 from .manual import ManualStrategy
+from .mixer import MixerStrategy
 from .random import RandomStrategy
 
 
@@ -11,6 +13,7 @@ class Types(enum.Enum):
     RANDOM = 1
     HYPNOX = 2
     DD3 = 3
+    MIXER = 4
 
 
 class Signal(enum.Enum):
@@ -31,6 +34,15 @@ def load(strategy, user=None):
 
     elif strategy.type == Types.DD3.value:
         stt = DD3Strategy.get_or_create(strategy=strategy)[0]
+
+    elif strategy.type == Types.MIXER.value:
+        stt = MixerStrategy.get_or_create(strategy=strategy)[0]
+        mixins = []
+        for st in strategy.mixins:
+            mixins.append(
+                {"strategy": load(core.db.Strategy.get_by_id(st.strategy_id)),
+                 "weight": st.weight})
+        stt.mixins = mixins
 
     else:
         raise ValueError("invalid strategy type")
---
+++ strategies/mixer/mixer.py
@@ -0,0 +1,84 @@
+import pandas
+import peewee
+
+import core
+from ..base import BaseStrategy
+
+
+class MixerIndicator(core.db.BaseModel):
+    indicator = peewee.ForeignKeyField(core.db.Indicator,
+                                       primary_key=True,
+                                       on_delete="CASCADE")
+    weighted_signal = peewee.DecimalField(default=0)
+
+
+class MixerStrategy(BaseStrategy):
+    buy_threshold = peewee.DecimalField(default=1)
+    sell_threshold = peewee.DecimalField(default=-1)
+
+    def get_indicators(self):
+        return super() \
+            .get_indicators() \
+            .select(core.db.Price, core.db.Indicator, MixerIndicator) \
+            .join(MixerIndicator, peewee.JOIN.LEFT_OUTER)
+
+    def get_indicators_df(self):
+        return super().get_indicators_df(self.get_indicators())
+
+    def refresh(self):
+        for mixin in self.mixins:
+            mixin["strategy"].refresh()
+
+        self.refresh_indicators()
+
+    def refresh_indicators(self):
+        BUY = core.strategies.Signal.BUY.value
+        NEUTRAL = core.strategies.Signal.NEUTRAL.value
+        SELL = core.strategies.Signal.SELL.value
+
+        indicators = pandas.DataFrame(self.get_indicators().dicts())
+        indicators.drop("signal", axis=1, inplace=True)
+        indicators["weighted_signal"] = NEUTRAL
+
+        for mixin in self.mixins:
+            mixind = pandas.DataFrame(
+                mixin["strategy"].get_indicators().dicts())
+            mixind["weight"] = mixin["weight"]
+            mixind["weighted_signal"] = mixind["signal"] * mixind["weight"]
+            indicators["weighted_signal"] += mixind["weighted_signal"]
+
+        indicators["signal"] = indicators["weighted_signal"].apply(
+            lambda x: BUY if x >= self.buy_threshold else
+            SELL if x <= self.sell_threshold else NEUTRAL)
+
+        # insert only new indicators
+        indicators = indicators.loc[indicators.indicator.isnull()]
+        if indicators.empty:
+            return
+
+        with core.db.connection.atomic():
+            indicators.strategy = self.strategy.id
+            indicators.price = indicators.id
+
+            core.db.Indicator.insert_many(
+                indicators[["strategy", "price", "signal"]]
+                .to_dict("records")
+            ).execute()
+
+            first = indicators[["strategy", "price", "signal"]].iloc[0]
+            first_id = core.db.Indicator.get(**first.to_dict()).id
+            indicators.indicator = range(first_id, first_id + len(indicators))
+
+            MixerIndicator.insert_many(
+                indicators[["indicator", "weighted_signal"]]
+                .to_dict("records")
+            ).execute()
+
+
+class MixedStrategies(core.db.BaseModel):
+    mixer_id = peewee.ForeignKeyField(core.db.Strategy,
+                                      on_delete="CASCADE",
+                                      backref="mixins")
+    strategy_id = peewee.ForeignKeyField(core.db.Strategy,
+                                         on_delete="CASCADE")
+    weight = peewee.DecimalField(default=1)
