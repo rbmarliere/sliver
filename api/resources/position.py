0d42aba335bf18ec6f3ab344148596597a556aed
Ricardo Marliere <rbmarliere@gmail.com>
2023-01-31 19:43:06 -0300

    api: vectorize GET /positions

--- api/resources/position.py
+++ api/resources/position.py
@@ -1,8 +1,11 @@
+from decimal import Decimal as D
+
 import pandas
 from flask_jwt_extended import get_jwt_identity, jwt_required
 from flask_restful import Resource, fields, marshal_with
 
 import core
+
 
 fields = {
     "id": fields.Integer,
@@ -22,6 +25,62 @@ fields = {
 }
 
 
+def quantize(row, col, prec_col):
+    return row[col].quantize(D("10") ** (D("-1") * row[prec_col]))
+
+
+def get_positions_df(user):
+    positions = pandas.DataFrame(user.get_positions().dicts())
+
+    if positions.empty:
+        return positions
+
+    positions["market"] = positions.base_ticker + "/" + positions.quote_ticker
+
+    base_precision = D("10") ** (D("-1") * positions.base_precision)
+    quote_precision = D("10") ** (D("-1") * positions.quote_precision)
+
+    positions.target_cost = positions.target_cost * quote_precision
+    positions.target_cost = positions.apply(
+        lambda x: quantize(x, "target_cost", "price_precision"), axis=1)
+
+    positions.entry_cost = positions.entry_cost * quote_precision
+    positions.entry_cost = positions.apply(
+        lambda x: quantize(x, "entry_cost", "price_precision"), axis=1)
+
+    positions.entry_amount = positions.entry_amount * base_precision
+    positions.entry_amount = positions.apply(
+        lambda x: quantize(x, "entry_amount", "base_precision"), axis=1)
+
+    positions.entry_price = positions.entry_price * quote_precision
+    positions.entry_price = positions.apply(
+        lambda x: quantize(x, "entry_price", "price_precision"), axis=1)
+
+    positions.exit_price = positions.exit_price * quote_precision
+    positions.exit_price = positions.apply(
+        lambda x: quantize(x, "exit_price", "price_precision"), axis=1)
+
+    positions.exit_amount = positions.exit_amount * base_precision
+    positions.exit_amount = positions.apply(
+        lambda x: quantize(x, "exit_amount", "base_precision"), axis=1)
+
+    positions.exit_cost = positions.exit_cost * quote_precision
+    positions.exit_cost = positions.apply(
+        lambda x: quantize(x, "exit_cost", "price_precision"), axis=1)
+
+    positions.fee = positions.fee * quote_precision
+    positions.fee = positions.apply(
+        lambda x: quantize(x, "fee", "price_precision"), axis=1)
+
+    positions.pnl = positions.pnl * quote_precision
+    positions.pnl = positions.apply(
+        lambda x: quantize(x, "pnl", "price_precision"), axis=1)
+
+    positions.roi = positions.roi.apply(lambda x: f"{x:.2f}%")
+
+    return positions
+
+
 class Position(Resource):
     @marshal_with(fields)
     @jwt_required()
@@ -29,31 +88,4 @@ class Position(Resource):
         uid = int(get_jwt_identity())
         user = core.db.User.get_by_id(uid)
 
-        positions = pandas.DataFrame(user.get_positions().dicts())
-
-        if positions.empty:
-            return
-
-        positions["market"] = positions.apply(
-            lambda x: core.utils.sformat(x), axis=1)
-        positions.target_cost = positions.apply(
-            lambda x: core.utils.qformat(x, "target_cost"), axis=1)
-        positions.entry_cost = positions.apply(
-            lambda x: core.utils.qformat(x, "entry_cost"), axis=1)
-        positions.entry_amount = positions.apply(
-            lambda x: core.utils.bformat(x, "entry_amount"), axis=1)
-        positions.entry_price = positions.apply(
-            lambda x: core.utils.qformat(x, "entry_price"), axis=1)
-        positions.exit_price = positions.apply(
-            lambda x: core.utils.qformat(x, "exit_price"), axis=1)
-        positions.exit_amount = positions.apply(
-            lambda x: core.utils.bformat(x, "exit_amount"), axis=1)
-        positions.exit_cost = positions.apply(
-            lambda x: core.utils.qformat(x, "exit_cost"), axis=1)
-        positions.fee = positions.apply(
-            lambda x: core.utils.qformat(x, "fee"), axis=1)
-        positions.pnl = positions.apply(
-            lambda x: core.utils.qformat(x, "pnl"), axis=1)
-        positions.roi = positions.roi.apply(lambda x: f"{x:.2f}%")
-
-        return positions.to_dict("records")
+        return get_positions_df(user).to_dict("records")
