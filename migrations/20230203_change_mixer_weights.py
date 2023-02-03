1238743aa4f07020b5fc2c8c016a6d9b77a6e498
Ricardo Marliere <rbmarliere@gmail.com>
2023-02-03 11:40:55 -0300

    migrate MixedStrategies weights

---
+++ migrations/20230203_change_mixer_weights.py
@@ -0,0 +1,19 @@
+import peewee
+from playhouse.migrate import PostgresqlMigrator, migrate
+
+import core
+import strategies
+
+
+migrator = PostgresqlMigrator(core.db.connection)
+
+weight = peewee.DecimalField(default=1)
+
+migrate(
+    migrator.rename_column(strategies.mixer.MixedStrategies._meta.table_name,
+                           "weight",
+                           "buy_weight"),
+    migrator.add_column(strategies.mixer.MixedStrategies._meta.table_name,
+                        "sell_weight",
+                        weight)
+)
