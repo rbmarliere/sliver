9b07da3a5ffdf084fae07b7f04158f4c9fb4a97d
Ricardo Marliere <rbmarliere@gmail.com>
2023-03-04 09:34:45 -0300

    core: stop_cooldown

---
+++ migrations/20230304_stop_cooldown.py
@@ -0,0 +1,19 @@
+import datetime
+
+import peewee
+from playhouse.migrate import PostgresqlMigrator, migrate
+
+import core
+
+
+next_refresh = peewee.DateTimeField(default=datetime.datetime.utcnow())
+
+migrator = PostgresqlMigrator(core.db.connection)
+
+stop_cooldown = peewee.IntegerField(default=1440)  # 1 day
+
+migrate(
+    migrator.add_column(core.db.TradeEngine._meta.table_name,
+                        "stop_cooldown",
+                        stop_cooldown),
+)
