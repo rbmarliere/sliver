e020e89d3843b5ef4839b1e2f31fb5ecb1e5bb64
Ricardo Marliere <rbmarliere@gmail.com>
2023-03-07 13:01:38 -0300

    core: add exchange column to Market

---
+++ migrations/20230307_market_exchange.py
@@ -0,0 +1,22 @@
+import peewee
+from playhouse.migrate import PostgresqlMigrator, migrate
+
+import core
+
+
+migrator = PostgresqlMigrator(core.db.connection)
+
+# set to not_null in db after migration
+exchange = peewee.ForeignKeyField(core.db.Exchange,
+                                  field=core.db.Exchange.id,
+                                  null=True)
+
+migrate(
+    migrator.add_column(core.db.Market._meta.table_name,
+                        "exchange_id",
+                        exchange),
+)
+
+for m in core.db.Market.select():
+    m.exchange = m.base.exchange
+    m.save()
