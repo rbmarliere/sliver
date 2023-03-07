3b2a4bfd9b221abafe25954549ee92debb61d661
Ricardo Marliere <rbmarliere@gmail.com>
2023-03-07 13:00:53 -0300

    core: add symbol column to Market

---
+++ migrations/20230306_market_symbol.py
@@ -0,0 +1,21 @@
+import peewee
+from playhouse.migrate import PostgresqlMigrator, migrate
+
+import core
+
+
+migrator = PostgresqlMigrator(core.db.connection)
+
+# set to not_null in db after migration
+symbol = peewee.TextField(null=True)
+
+migrate(
+    migrator.add_column(core.db.Market._meta.table_name,
+                        "symbol",
+                        symbol),
+)
+
+for m in core.db.Market.select():
+    m.symbol = m.get_symbol()
+    print("{i} - {s}".format(i=m.id, s=m.symbol))
+    m.save()
