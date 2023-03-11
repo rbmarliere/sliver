38ece139fda708b6128f32053e32b6ed4faeef8d
Ricardo Marliere <rbmarliere@gmail.com>
2023-03-11 16:54:52 -0300

    scripts: new telethon_auth.py

---
+++ scripts/telethon_auth.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import argparse
+import asyncio
+import os
+
+from telethon import TelegramClient
+
+import core
+
+
+session = core.config["LOGS_DIR"] + "/telethon"
+api_id = core.config["TELEGRAM_API_ID"]
+api_hash = core.config["TELEGRAM_API_HASH"]
+# bot_token = core.config["TELEGRAM_BOT_TOKEN"]
+
+
+async def connect(session):
+    if os.path.exists(session + ".session"):
+        print("Session file exists")
+        return
+
+    client = TelegramClient(session, api_id, api_hash)
+    # client.start(bot_token=bot_token)
+    await client.start()
+    # await client.log_out()
+
+
+if __name__ == "__main__":
+    argp = argparse.ArgumentParser()
+    argp.add_argument("-s", "--session", help="Session name")
+    args = argp.parse_args()
+
+    if args.session:
+        session = args.session
+
+    asyncio.run(connect(session))
