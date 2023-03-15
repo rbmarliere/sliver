37c6df809eb1949316b814fc6b77cb51e5039014
Ricardo Marliere <rbmarliere@gmail.com>
2023-03-15 08:30:39 -0300

    better debug config + automation

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+vim.fn.jobstart("ng serve -c development --host 0.0.0.0", { cwd = "web" })
+
 require("lspconfig").pylsp.setup({
   -- organizeImports requires pylsp-rope in mason env
   on_attach = LSPAttach,
@@ -8,9 +10,10 @@ require("lspconfig").pylsp.setup({
 local dap = require("dap")
 
 dap.defaults.fallback.force_external_terminal = true
+
 dap.defaults.fallback.external_terminal = {
   command = "/usr/local/bin/alacritty",
-  args = { "-e" },
+  args = { "--hold", "-e" },
 }
 
 dap.adapters.firefox = {
@@ -19,34 +22,49 @@ dap.adapters.firefox = {
   args = { vim.fn.stdpath("data") .. "/mason/packages/firefox-debug-adapter/dist/adapter.bundle.js" },
 }
 
+local typescript = {
+  name = "Launch Firefox",
+  type = "firefox",
+  request = "launch",
+  url = "http://localhost:4200",
+  webRoot = "${workspaceFolder}/web",
+  firefoxExecutable = "/usr/bin/firefox",
+  profile = "debug",
+  keepProfileChanges = true,
+}
 dap.configurations.typescript = {
-  {
-    name = "Launch Firefox",
-    type = "firefox",
-    request = "launch",
-    url = "http://localhost:4200",
-    webRoot = "${workspaceFolder}/web",
-    firefoxExecutable = "/usr/bin/firefox",
-    profile = "debug",
-    keepProfileChanges = true,
-  }
+  typescript,
 }
 
 require("dap-python").setup(vim.fn.getcwd() .. "/venv/bin/python", { console = "externalTerminal" })
 
-table.insert(dap.configurations.python, {
+local api = {
   type = "python",
   request = "launch",
   name = "API",
   program = "api",
   args = { "--no-debug" },
   console = "externalTerminal",
-})
+}
+table.insert(dap.configurations.python, api)
 
-table.insert(dap.configurations.python, {
+local watchdog = {
   type = "python",
   request = "launch",
   name = "Watchdog",
   program = "core",
   console = "externalTerminal",
+}
+table.insert(dap.configurations.python, watchdog)
+
+table.insert(dap.configurations.python, {
+  type = "python",
+  request = "launch",
+  name = "Stream",
+  program = "strategies/hypnox/twitter.py",
+  console = "externalTerminal",
 })
+
+dap.run(typescript)
+dap.run(api)
+dap.run(watchdog)
