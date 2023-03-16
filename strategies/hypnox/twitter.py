be3c7036eaccff19e61b4da97f60c3e6356997ff
Ricardo Marliere <rbmarliere@gmail.com>
2023-03-16 16:54:19 -0300

    hypnox: new HypnoxUser table

--- strategies/hypnox/twitter.py
+++ strategies/hypnox/twitter.py
@@ -1,6 +1,5 @@
 import argparse
 import datetime
-import os
 import re
 import ssl
 
@@ -14,64 +13,7 @@ import core
 import strategies
 
 
-TRACKED_USERS = [
-    "22loops", "Amdtrades", "Ameba_NM", "Anbessa100", "AnondranCrypto",
-    "AstroCryptoGuru", "Astrones2", "BITCOINTRAPPER", "BTC3P0", "BTCVIX",
-    "BTC_JackSparrow", "BTC_y_tho", "Benaskren", "BigCheds", "BigChonis",
-    "BitBitCrypto", "Bit_Fink", "BitcoinMunger", "BitcoinRiot", "BitcoinTina",
-    "BrencJ", "BullChain", "ByzGeneral", "CL207", "CRYPT0HULK",
-    "CanteringClark", "CarpeNoctom", "CastilloTrading", "ChartAlertsIO",
-    "ChartVampire", "ChartsBtc", "CleverCryptoDog", "CobraBitcoin",
-    "ColdBloodShill", "CosmonautC", "CredibleCrypto", "Crypt0Entropy",
-    "Crypt0_kenny", "CryptoBoss1984", "CryptoCapo", "CryptoCharles__",
-    "CryptoCourage1", "CryptoKaleo", "CryptoKea", "CryptoLeos", "CryptoLimbo_",
-    "CryptoMaestro", "CryptoMichNL", "CryptoNewton", "CryptoParadyme",
-    "CryptoShadowOff", "CryptoTrooper_", "CryptoUB", "CryptoWizardd",
-    "Crypto_Gambit_", "Crypto_Horseman", "Crypto_Jeremiah", "Crypto_Scofield",
-    "Cryptorphic1", "DTCcryptotrades", "DaanCrypto", "DanCrypto",
-    "DaveCrypto83", "EmperorBTC", "FangTrades", "FeraSY1", "FizeekMoney",
-    "Flex__Trades", "FlyBull3", "George1Trader", "Glimmerycoin",
-    "HackermanAce", "Hayess5178", "HerroCrypto", "HsakaTrades", "HxroDaily",
-    "IamBitmannn", "IamCryptoWolf", "IchimokuScholar", "ImNotTheWolf",
-    "IncomeSharks", "IrnCrypt", "J0hnnyw00", "JJcycles", "Jsterz", "KRTrades_",
-    "KevinSvenson_", "KoroushAK", "LLLuckyl", "LightCrypto", "Lord_Ashdrake",
-    "MMcrypto", "MacnBTC", "MacroCRG", "Mesawine1", "Mojo_Crypto_BTC",
-    "MoonOverlord", "Murfski", "MuroCrypto", "NinjaCryptoCoin", "Ninjascalp",
-    "OtsukimiCrypto", "Pentosh1", "PhilakoneCrypto", "PositiveCrypto",
-    "Psolemn", "QuantRob", "RodMaartin", "RookieXBT", "RyanDraycott",
-    "SMtrades_", "SalsaTekila", "Satosheye", "ShardiB2", "SmartContracter",
-    "SnatchProfits", "StackinBits", "StockCats", "SwenLink", "THE_FLASH_G",
-    "TXWestCapital", "Teaching_Crypto", "TheCryptoDog", "TheCryptomist",
-    "TheEuroSniper", "TheMoonCarl", "ThePsychoBot", "TheSeanNance",
-    "TheTradingHubb", "ThinkingUSD", "Timeless_Crypto", "Trader2000X",
-    "TraderAditya", "TraderKoz", "TraderMagus", "TraderReno", "Trader_xB",
-    "Tradermayne", "TradingMotives", "TradySlim", "TrendSpider",
-    "TrueCrypto28", "UltraXBT", "VEGETACRYPTO1", "Workedia", "Yodaskk",
-    "alanizBTC", "alpha_algo", "anndylian", "ape_rture", "balajis",
-    "bearshakalaka", "btcharlatan", "bullshakalaka", "caprioleio", "cburniske",
-    "chartstreamer", "coinpocalypse", "coinstechnical", "crypto_Off",
-    "crypto_birb", "crypto_iso", "crypto_mak", "cryptodude999", "cryptokita",
-    "cryptolimbo_", "cryptomagnified", "cryptomeowmeow", "cryptosham",
-    "cryptowhitewalk", "cryptoyoda1338", "cyrii_MM", "davthewave", "devchart",
-    "digitalikNet", "easyeight08", "edwardmorra_btc", "ericjuta", "filbfilb",
-    "fozcrypto", "galaxyBTC", "glassnode", "greektoshi", "h_bitcoiner",
-    "high_fades", "im_goomba", "imkeshav", "j0hnw00", "jackis_trader",
-    "jimtalbot", "joshnomics", "loomdart", "lunarCRUSH", "lunatictrader1",
-    "mark_cullen", "mason_jang", "moonshilla", "mysteryta47",
-    "nebraskangooner", "oilermanhockey", "parabolictrav", "polar_hunt",
-    "pushpendrakum", "raticoin1", "realadamli", "redxbt", "rektcapital",
-    "santimentfeed", "satsbuyer", "scottmelker", "singhsoro", "squatch_crypto",
-    "stonXBT", "teaching_crypto", "thalamu_", "thecryptomars", "theo_crypto99",
-    "therealgoldbug1", "thetaseek", "tmttraders", "trader1sz", "vortexics82",
-    "walter_wyckoff", "xxstevelee"
-]
-
-
 class Stream(tweepy.StreamingClient):
-
-    def on_connection_error(self):
-        core.watchdog.warning("connection error")
-        super().on_connection_error()
 
     def on_disconnect(self):
         core.watchdog.warning("disconnected")
@@ -80,14 +22,6 @@ class Stream(tweepy.StreamingClient):
     def on_exception(self, exception):
         core.watchdog.error("got exception", exception)
         super().on_exception(exception)
-
-    def on_errors(self, errors):
-        core.watchdog.error("got errors", errors)
-        super().on_errors(errors)
-
-    def on_request_error(self, status_code):
-        core.watchdog.error("got request error", status_code)
-        super().on_request_error(status_code)
 
     def on_tweet(self, status):
         # ignore replies
@@ -136,17 +70,28 @@ class Stream(tweepy.StreamingClient):
 
 
 def get_uids():
-    uids = []
     client = tweepy.Client(core.config["TWITTER_BEARER_TOKEN"])
 
-    for username in TRACKED_USERS:
-        user = client.get_user(username=username)
-        if user.errors:
-            core.watchdog.info("user {u} not found".format(u=user))
-            continue
-        uids.append(user.data.id)
+    pending = core.strategies.hypnox. \
+        HypnoxUser \
+        .select() \
+        .where(core.strategies.hypnox.HypnoxUser.twitter_user_id.is_null())
 
-    return uids
+    for user in pending:
+        t_user = client.get_user(username=user.username)
+
+        if not t_user.data:
+            core.watchdog.info("user {u} not found".format(u=user.username))
+            continue
+
+        user.twitter_user_id = t_user.data.id
+        user.username = t_user.data.username
+        user.save()
+
+    u = [u.twitter_user_id for u in core.strategies.hypnox.HypnoxUser.select()]
+    if len(u) == 0:
+        raise Exception("no users found")
+    return u
 
 
 def get_rules(uids):
@@ -171,8 +116,7 @@ def get_rules(uids):
 
 def stream():
     argp = argparse.ArgumentParser()
-    argp.add_argument("--rules", action="store_true")
-    argp.add_argument("--users", action="store_true")
+    argp.add_argument("--reset", action="store_true")
     args = argp.parse_args()
 
     core.watchdog.set_logger("stream")
@@ -182,21 +126,9 @@ def stream():
 
     stream = Stream(core.config["TWITTER_BEARER_TOKEN"])
 
-    core.watchdog.info("reading users")
-    uids_file = core.config["LOGS_DIR"] + "/user_ids.txt"
-    if args.users or not os.path.isfile(uids_file):
-        core.watchdog.info("storing user ids")
-        try:
-            os.remove(uids_file)
-        except FileNotFoundError:
-            pass
+    if args.reset:
+        core.watchdog.info("resetting users and stream rules")
         uids = get_uids()
-    else:
-        uids = open(uids_file, "r").read().splitlines()
-
-    core.watchdog.info("reading rules")
-    if args.rules:
-        core.watchdog.info("storing rules")
 
         curr_rules = stream.get_rules()
         if curr_rules.data:
