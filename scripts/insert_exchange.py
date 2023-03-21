4d3d47bf652a51df4f195e44538cf2dc0770c2c5
Ricardo Marliere <rbmarliere@gmail.com>
2023-03-21 17:38:20 -0300

    scripts: fix insert_exchange

--- scripts/insert_exchange.py
+++ scripts/insert_exchange.py
@@ -13,27 +13,44 @@ if __name__ == "__main__":
                       "--exchange-name",
                       help="exchange name to fetch from",
                       required=True)
+    argp.add_argument("-t",
+                      "--type",
+                      help="exchange type",
+                      type=int,
+                      required=True)
+    argp.add_argument("--api-endpoint",
+                      help="exchange api endpoint")
+    argp.add_argument("--api-sandbox-endpoint",
+                      help="exchange api sandbox endpoint")
     args = argp.parse_args()
 
-    try:
-        exchange = getattr(ccxt, args.exchange_name)({})
-    except AttributeError:
-        print("exchange not supported by ccxt")
-        sys.exit(1)
-
-    l1 = set([i for i in exchange.timeframes.keys()])
-    l2 = set(core.utils.get_timeframes())
-    timeframes = str(list(l1 & l2))
-    if not timeframes:
-        timeframes = ['15m', '30m', '4h', '1h', '1w', '1d']
+    timeframes = ['15m', '30m', '4h', '1h', '1w', '1d']
 
     obj = {
         "name": args.exchange_name,
-        "rate_limit": exchange.rateLimit,
-        "precision_mode": exchange.precisionMode,
-        "padding_mode": exchange.paddingMode,
-        "timeframes": str(list(l1 & l2))
+        "rate_limit": 1000,
+        "precision_mode": 0,
+        "padding_mode": 0,
+        "timeframes": timeframes,
+        "api_endpoint": args.api_endpoint,
+        "api_sandbox_endpoint": args.api_sandbox_endpoint
     }
+
+    if args.type == core.exchanges.Types.CCXT:
+        try:
+            exchange = getattr(ccxt, args.exchange_name)({})
+        except AttributeError:
+            print("exchange not supported by ccxt")
+            sys.exit(1)
+
+        l1 = set([i for i in exchange.timeframes.keys()])
+        l2 = set(core.utils.get_timeframes())
+        timeframes = str(list(l1 & l2))
+        if timeframes:
+            obj["timeframes"] = timeframes
+
+        obj["precision_mode"] = exchange.precisionMode
+        obj["padding_mode"] = exchange.paddingMode
 
     exchange = core.db.Exchange(**obj)
 
