6e9b4c015e13a422f19cfa37515e86c641339585
Ricardo Marliere <rbmarliere@gmail.com>
2023-03-21 13:57:49 -0300

    cleanup scripts dir

--- scripts/backup_db.sh
+++ scripts/backup_db.sh
@@ -2,8 +2,11 @@
 
 DB_USER=""
 DB_NAME=""
-DB_HOST=""
-BKP_DIR=""
+DB_HOST="192.168.1.169"
+BKP_DIR="$HOME/sliver.db"
+TELEGRAM_BOT_ID=""
+TELEGRAM_CHAT_ID=""
+TELEGRAM_API="https://api.telegram.org/bot$TELEGRAM_BOT_ID/sendMessage?chat_id=$TELEGRAM_CHAT_ID&text=$(hostname):%20"
 
 # grab first argument of the script
 # if it's not empty, use it as the backup directory
@@ -17,14 +20,16 @@ pushd "$BKP_DIR" || exit 1
 
 CURRENT=$(date +%Y%m%d%H%M%S).tar
 
-if pg_dump -h "$DB_HOST" -U "$DB_USER" -F t "$DB_NAME" > "$CURRENT"; then
-    # delete all but the last 5 backups
-    ls -t | tail -n +7 | xargs rm -f
+if pg_dump -h "$DB_HOST" -U "$DB_USER" -F t "$DB_NAME" > "$CURRENT" 2>&1; then
+    # delete all but the last 35 backups
+    ls -t | tail -n +35 | xargs rm -f
 
     if [ -L current ]; then
         unlink current
     fi
     ln -s "$CURRENT" current
+else
+    curl -s "$TELEGRAM_API"database%20backup%20failed
 fi
 
 popd || exit 1
