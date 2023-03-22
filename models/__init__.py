f90cd7dd6b5938911764a8ae31f3da27068c4dbc
Ricardo Marliere <rbmarliere@gmail.com>
2023-03-22 14:26:14 -0300

    models: reformat using black

--- models/__init__.py
+++ models/__init__.py
@@ -31,8 +31,7 @@ def load_model(model_name):
 
     print("loading model {m}".format(m=model_name))
 
-    modelpath = pathlib.Path(core.config["MODELS_DIR"] + "/" +
-                             model_name).resolve()
+    modelpath = pathlib.Path(core.config["MODELS_DIR"] + "/" + model_name).resolve()
     if not modelpath.exists():
         print("model {m} does not exist".format(m=model_name))
         raise core.errors.ModelDoesNotExist
@@ -46,12 +45,10 @@ def load_model(model_name):
         try:
             model = model_module.load_model(modelpath)
         except tensorflow.errors.ResourceExhaustedError:
-            print("model {m} is too large to be loaded"
-                  .format(m=model_name))
+            print("model {m} is too large to be loaded".format(m=model_name))
             raise core.errors.ModelTooLarge
     except Exception as e:
-        print("could not load model {m}".format(m=model_name),
-              exception=e)
+        print("could not load model {m}".format(m=model_name), exception=e)
         raise core.errors.ModelDoesNotExist
 
     model.config = model_module.config
