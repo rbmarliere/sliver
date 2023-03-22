3191edb736da7dc87e80c77562312654024c7fb5
Ricardo Marliere <rbmarliere@gmail.com>
2023-03-22 14:21:52 -0300

    exch: refactor Order.sync + small fixes

--- exchanges/ccxt.py
+++ exchanges/ccxt.py
@@ -1,25 +1,23 @@
 import datetime
 import time
 
+import core
 import pandas
 
 import ccxt
-import core
-from .base import BaseExchange
 
+from .base import BaseExchange
 
 print = core.watchdog.Watchdog().print
 
 
 class CCXT(BaseExchange):
-
     @BaseExchange.api.setter
     def api(self, credential=None):
         api_key = {}
 
         if credential:
-            api_key = {"apiKey": credential.api_key,
-                       "secret": credential.api_secret}
+            api_key = {"apiKey": credential.api_key, "secret": credential.api_secret}
 
         ccxt_class = getattr(ccxt, self.exchange.name)
         self._api = ccxt_class(api_key)
@@ -45,8 +43,7 @@ class CCXT(BaseExchange):
                 if self.market:
                     sleep_time = self.market.base.exchange.rate_limit
 
-                print("rate limited, sleeping for {s} seconds"
-                      .format(s=sleep_time))
+                print("rate limited, sleeping for {s} seconds".format(s=sleep_time))
                 time.sleep(sleep_time)
 
                 return inner(self, *args, **kwargs)
@@ -89,24 +86,19 @@ class CCXT(BaseExchange):
         return self._api.fetch_ticker(symbol)["last"]
 
     @api_call
-    def api_fetch_ohlcv(self,
-                        symbol,
-                        timeframe,
-                        since=None,
-                        limit=500,
-                        page_size=500):
+    def api_fetch_ohlcv(self, symbol, timeframe, since=None, limit=500, page_size=500):
         if since:
             since = int(since.timestamp() * 1000)
 
-        ohlcv = self._api.fetch_ohlcv(symbol,
-                                      timeframe=timeframe,
-                                      since=since,
-                                      limit=limit)
+        ohlcv = self._api.fetch_ohlcv(
+            symbol, timeframe=timeframe, since=since, limit=limit
+        )
 
         df = pandas.DataFrame.from_records(ohlcv)
         df.columns = ["time", "open", "high", "low", "close", "volume"]
-        df.time = pandas.to_datetime(df.time, origin="unix", unit="ms") \
-            .dt.tz_localize(None)
+        df.time = pandas.to_datetime(df.time, origin="unix", unit="ms").dt.tz_localize(
+            None
+        )
 
         return df
 
@@ -139,11 +131,7 @@ class CCXT(BaseExchange):
     @api_call
     def api_create_order(self, symbol, type, side, amount, price=None):
         try:
-            new_order = self._api.create_order(symbol,
-                                               type,
-                                               side,
-                                               amount,
-                                               price)
+            new_order = self._api.create_order(symbol, type, side, amount, price)
 
             return new_order["id"]
 
@@ -158,34 +146,106 @@ class CCXT(BaseExchange):
 
             time.sleep(10)
 
-            open_orders = self.api_cancel_orders(symbol,
-                                                 status="open")
+            open_orders = self.api_fetch_orders(symbol, status="open")
             if open_orders:
                 last_order = open_orders[-1]
                 last_cost = int(last_order["price"] * last_order["amount"])
                 cost = int(price * amount)
-                trade_dt = datetime.datetime.strptime(last_order["datetime"],
-                                                      "%Y-%m-%dT%H:%M:%S.%fZ")
+                trade_dt = datetime.datetime.strptime(
+                    last_order["datetime"], "%Y-%m-%dT%H:%M:%S.%fZ"
+                )
                 # check if trade was made within 2min
-                if trade_dt > (datetime.datetime.utcnow()
-                               - datetime.timedelta(minutes=2)) \
-                        and cost > last_cost*0.999 and cost < last_cost*1.001 \
-                        and int(price) == int(last_order["price"]):
+                if (
+                    trade_dt
+                    > (datetime.datetime.utcnow() - datetime.timedelta(minutes=2))
+                    and cost > last_cost * 0.999
+                    and cost < last_cost * 1.001
+                    and int(price) == int(last_order["price"])
+                ):
                     print("order was created and is open")
                     return last_order["id"]
 
-            closed_orders = self.api_fetch_orders(symbol,
-                                                  status="closed")
+            closed_orders = self.api_fetch_orders(symbol, status="closed")
             if closed_orders:
                 last_order = closed_orders[-1]
                 last_cost = int(last_order["price"] * last_order["amount"])
                 cost = int(price * amount)
-                trade_dt = datetime.datetime.strptime(last_order["datetime"],
-                                                      "%Y-%m-%dT%H:%M:%S.%fZ")
+                trade_dt = datetime.datetime.strptime(
+                    last_order["datetime"], "%Y-%m-%dT%H:%M:%S.%fZ"
+                )
                 # check if trade was made within 2min
-                if trade_dt > (datetime.datetime.utcnow()
-                               - datetime.timedelta(minutes=2)) \
-                        and cost > last_cost*0.999 and cost < last_cost*1.001 \
-                        and int(price) == int(last_order["price"]):
+                if (
+                    trade_dt
+                    > (datetime.datetime.utcnow() - datetime.timedelta(minutes=2))
+                    and cost > last_cost * 0.999
+                    and cost < last_cost * 1.001
+                    and int(price) == int(last_order["price"])
+                ):
                     print("order was created and is closed")
                     return last_order["id"]
+
+    @api_call
+    def get_synced_order(self, oid) -> core.db.Order:
+        order = core.db.Order.get_or_none(exchange_order_id=oid)
+        if order is None or order.position_id != self.position.id:
+            order = core.db.Order()
+
+        ex_order = self.api_fetch_orders(self.market.get_symbol(), oid=oid)
+
+        order.position = self.position
+        order.exchange_order_id = ex_order["id"]
+        order.status = ex_order["status"]
+        order.type = ex_order["type"]
+        order.side = ex_order["side"]
+        order.time = ex_order["datetime"]
+        if not ex_order["datetime"]:
+            order.time = datetime.datetime.utcnow()
+
+        price = ex_order["average"]
+        amount = ex_order["amount"]
+        filled = ex_order["filled"]
+        if not filled:
+            filled = 0
+        cost = ex_order["cost"]
+        if not cost:
+            cost = filled * amount
+
+        # transform values to db entry standard
+        order.price = self.market.quote.transform(price)
+        order.amount = self.market.base.transform(amount)
+        order.cost = self.market.quote.transform(cost)
+        order.filled = self.market.base.transform(filled)
+
+        implicit_cost = self.market.base.format(amount) * order.price
+        print(
+            "{a} @ {p} ({c})".format(
+                a=self.market.base.print(amount),
+                p=self.market.quote.print(price),
+                c=self.market.quote.print(implicit_cost),
+            )
+        )
+
+        print("filled: {f}".format(f=self.market.base.print(filled)))
+
+        # check for fees
+        if ex_order["fee"] is None:
+            # TODO maybe fetch from difference between insertion & filled
+            order.fee = 0
+        else:
+            try:
+                order.fee = self.market.quote.transform(ex_order["fee"]["cost"])
+            except KeyError:
+                order.fee = 0
+
+        if not order.status:
+            if self.id:
+                if filled == cost:
+                    order.status = "closed"
+                else:
+                    order.status = "canceled"
+            else:
+                order.status = "open"
+
+        order.save()
+
+        return order
