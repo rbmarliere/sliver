eebdb2ac1f207676a081bfa5b9418a149e653173
Ricardo Marliere <rbmarliere@gmail.com>
2023-03-28 14:41:56 -0300

    use strategy factory in indicator resource

--- src/sliver/api/resources/indicator.py
+++ src/sliver/api/resources/indicator.py
@@ -3,17 +3,18 @@ from flask_restful import Resource, marshal
 
 from sliver.api.exceptions import StrategyDoesNotExist
 from sliver.api.resources.fields import get_fields
-from sliver.strategy import BaseStrategy as Strategy
+from sliver.strategies.factory import StrategyFactory
+from sliver.strategy import BaseStrategy
 
 
 class Indicator(Resource):
     @jwt_required()
     def get(self, strategy_id):
         try:
-            strategy = Strategy.get_by_id(strategy_id)
+            strategy = StrategyFactory.from_base(BaseStrategy.get_by_id(strategy_id))
             if strategy.deleted:
                 raise StrategyDoesNotExist
-        except Strategy.DoesNotExist:
+        except BaseStrategy.DoesNotExist:
             raise StrategyDoesNotExist
 
         ind = strategy.get_indicators_df()
