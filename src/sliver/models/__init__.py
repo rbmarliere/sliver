554da51bca3b86834aa2928ff0b580a1fff8beae
Ricardo Marliere <rbmarliere@gmail.com>
2023-03-29 17:08:05 -0300

    fix gitignore

---
+++ src/sliver/models/__init__.py
@@ -0,0 +1,71 @@
+import importlib
+import pathlib
+import sys
+
+import tensorflow
+import transformers
+
+from sliver.config import Config
+from sliver.exceptions import ModelDoesNotExist, ModelTooLarge
+from sliver.print import print
+
+transformers.logging.set_verbosity_error()
+tensorflow.get_logger().setLevel("INFO")
+
+models = []
+
+
+def get_model(model_name):
+    model_module = importlib.import_module("models." + model_name)
+    model = model_module.get_model()
+    model.config = model_module.config
+    model.tokenizer = model_module.load_tokenizer()
+    return model
+
+
+def load_model(model_name):
+    for model in models:
+        if model.config["name"] == model_name:
+            return model
+
+    print("loading model {m}".format(m=model_name))
+
+    modelpath = pathlib.Path(Config().MODELS_DIR + "/" + model_name).resolve()
+    if not modelpath.exists():
+        print("model {m} does not exist".format(m=model_name))
+        raise ModelDoesNotExist
+
+    model_module = importlib.import_module("sliver.models." + model_name)
+
+    try:
+        model = model_module.load_model(modelpath)
+    except tensorflow.errors.ResourceExhaustedError:
+        del models[0]
+        try:
+            model = model_module.load_model(modelpath)
+        except tensorflow.errors.ResourceExhaustedError:
+            print("model {m} is too large to be loaded".format(m=model_name))
+            raise ModelTooLarge
+    except Exception as e:
+        print("could not load model {m}".format(m=model_name), exception=e)
+        raise ModelDoesNotExist
+
+    model.config = model_module.config
+    model.tokenizer = model_module.load_tokenizer(modelpath=modelpath)
+
+    models.append(model)
+
+    return model
+
+
+def import_model(model_name):
+    try:
+        return getattr(sys.modules[__name__], model_name)
+    except AttributeError:
+        pass
+
+    model = load_model(model_name)
+
+    setattr(sys.modules[__name__], model_name, model)
+
+    return model
