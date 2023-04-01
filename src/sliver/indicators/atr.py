aeae9c00de8a4da8dd82817056c431e8a739d26f
Ricardo Marliere <rbmarliere@gmail.com>
2023-04-01 10:39:17 -0300

    atr: iterrows and fillna

--- src/sliver/indicators/atr.py
+++ src/sliver/indicators/atr.py
@@ -12,8 +12,8 @@ def ATR(ohlc, length=14, smoothing="sma"):
         alpha = 1 / length
         first = df.tr.loc[: length - 1].mean()
         df.loc[length - 1, "atr"] = first
-        for idx, row in df.tr.loc[length:].iteritems():
-            df.loc[idx, "atr"] = alpha * row + (1 - alpha) * df.atr.loc[idx - 1]
+        for idx, row in df.loc[length:].iterrows():
+            df.loc[idx, "atr"] = alpha * row.tr + (1 - alpha) * df.atr.loc[idx - 1]
 
     elif smoothing == "ema":
         df["atr"] = df.tr.ewm(span=length).mean()
@@ -23,5 +23,8 @@ def ATR(ohlc, length=14, smoothing="sma"):
         raise ValueError("invalid smoothing method")
 
     df.drop(["tr0", "tr1", "tr2", "tr"], axis=1, inplace=True)
+
+    # fill NaN with 0
+    df.atr.fillna(0, inplace=True)
 
     return df
