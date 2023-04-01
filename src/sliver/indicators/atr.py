00da65104ba8710c6195b8e688c29559bd41f2d6
Ricardo Marliere <rbmarliere@gmail.com>
2023-04-01 10:33:46 -0300

    ATR indicator, with tradingview matched RMA

---
+++ src/sliver/indicators/atr.py
@@ -0,0 +1,27 @@
+def ATR(ohlc, length=14, smoothing="sma"):
+    df = ohlc.copy()
+
+    df["tr0"] = abs(df.high - df.low)
+    df["tr1"] = abs(df.high - df.close.shift(1))
+    df["tr2"] = abs(df.close.shift(1) - df.low)
+
+    df["tr"] = df[["tr0", "tr1", "tr2"]].max(axis=1)
+
+    if smoothing == "rma":
+        # https://www.tradingview.com/pine-script-reference/v5/#fun_ta{dot}rma
+        alpha = 1 / length
+        first = df.tr.loc[: length - 1].mean()
+        df.loc[length - 1, "atr"] = first
+        for idx, row in df.tr.loc[length:].iteritems():
+            df.loc[idx, "atr"] = alpha * row + (1 - alpha) * df.atr.loc[idx - 1]
+
+    elif smoothing == "ema":
+        df["atr"] = df.tr.ewm(span=length).mean()
+    elif smoothing == "sma":
+        df["atr"] = df.tr.rolling(length).mean()
+    else:
+        raise ValueError("invalid smoothing method")
+
+    df.drop(["tr0", "tr1", "tr2", "tr"], axis=1, inplace=True)
+
+    return df
