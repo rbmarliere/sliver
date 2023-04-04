cbc2a185d6a38c5ffaaadb0c3977a8afa7f1af35
Ricardo Marliere <rbmarliere@gmail.com>
2023-04-04 13:24:57 -0300

    add load=True to IModel.__init__

--- src/sliver/models/__init__.py
+++ src/sliver/models/__init__.py
@@ -1,5 +1,4 @@
 import importlib
-import pathlib
 
 import tensorflow
 
@@ -13,15 +12,10 @@ def get(model_name):
     try:
         model_module = importlib.import_module(f"sliver.models.{model_name}")
         model_class = getattr(model_module, model_name)
-        model_obj = model_class()
     except (ModuleNotFoundError, AttributeError):
         raise DisablingError(f"model {model_name} does not exist")
 
-    modelpath = pathlib.Path(model_obj.path).resolve()
-    if not modelpath.exists():
-        raise DisablingError(f"model {model_name} does not exist")
-
-    return model_obj
+    return model_class
 
 
 def load(model_name):
@@ -31,14 +25,14 @@ def load(model_name):
 
     print(f"loading model {model_name}")
 
-    model_obj = get(model_name)
+    model_class = get(model_name)
 
     try:
-        model = model_obj.load()
+        model = model_class()
     except tensorflow.errors.ResourceExhaustedError:
         del models[0]
         try:
-            model = model_obj.load()
+            model = model_class()
         except tensorflow.errors.ResourceExhaustedError:
             raise DisablingError(f"model {model_name} is too large to be loaded")
     except Exception:
