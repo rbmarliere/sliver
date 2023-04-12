6b8846b719092dde343db8f4d6c5f2e23c0a97c4
Ricardo Marliere <rbmarliere@gmail.com>
2023-04-12 16:11:06 -0300

    manual strategy get_signal override

--- src/sliver/strategies/manual.py
+++ src/sliver/strategies/manual.py
@@ -6,6 +6,9 @@ from sliver.strategy import IStrategy
 class ManualStrategy(IStrategy):
     signal = peewee.DecimalField(default=0)
 
+    def get_signal(self):
+        return self.signal
+
     def refresh_indicators(self, indicators, signal=None):
         if signal:
             self.signal = signal
