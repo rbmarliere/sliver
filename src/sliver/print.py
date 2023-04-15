fbebf55aac7fc86aa04ab19237b18ecd5840b9ba
Ricardo Marliere <rbmarliere@gmail.com>
2023-04-15 09:48:48 -0300

    fix print.py

--- src/sliver/print.py
+++ src/sliver/print.py
@@ -1,4 +1,4 @@
-def print(message=None, exception=None):
+def print(message=None, exception=None, notice=False):
     from .watchdog import Watchdog
 
-    return Watchdog().print(message, exception)
+    return Watchdog().print(message, exception, notice)
