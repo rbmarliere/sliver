0b79b0db48fcc1dbd7465a64c7fc011e951514d2
Ricardo Marliere <rbmarliere@gmail.com>
2023-04-26 14:26:21 -0300

    api: add since, until args to GET /indicators

--- src/sliver/api/resources/indicator.py
+++ src/sliver/api/resources/indicator.py
@@ -5,11 +5,17 @@ from flask_restful import Resource, marshal
 from sliver.api.exceptions import StrategyDoesNotExist
 from sliver.strategies.factory import StrategyFactory
 from sliver.strategy import BaseStrategy
+from flask_restful import reqparse
 
 
 class Indicator(Resource):
     @jwt_required()
     def get(self, strategy_id):
+        parser = reqparse.RequestParser()
+        parser.add_argument("since", type=int, default=None, location="args")
+        parser.add_argument("until", type=int, default=None, location="args")
+        args = parser.parse_args()
+
         try:
             strategy = StrategyFactory.from_base(BaseStrategy.get_by_id(strategy_id))
             if strategy.deleted:
@@ -17,7 +23,9 @@ class Indicator(Resource):
         except BaseStrategy.DoesNotExist:
             raise StrategyDoesNotExist
 
-        ind = strategy.get_indicators_df(join_type=peewee.JOIN.INNER)
+        ind = strategy.get_indicators_df(
+            join_type=peewee.JOIN.INNER, since=args.since, until=args.until
+        )
 
         if not ind.empty:
             ind.replace({float("nan"): None}, inplace=True)
