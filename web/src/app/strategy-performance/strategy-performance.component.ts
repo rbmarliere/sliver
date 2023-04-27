430bb0bf069ddfa860fd8c6c29e701b7f3183cb1
Ricardo Marliere <rbmarliere@gmail.com>
2023-04-27 15:48:54 -0300

    web: new /strategy/id/performance route

--- web/src/app/strategy-performance/strategy-performance.component.ts
+++ web/src/app/strategy-performance/strategy-performance.component.ts
@@ -1,34 +1,72 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Engine } from '../engine';
+import { EngineService } from '../engine.service';
+import { IndicatorService } from '../indicator.service';
 import { Metrics } from '../metrics';
 import { Position } from '../position';
 import { PositionService } from '../position.service';
 import { Strategy } from '../strategy';
+import { StrategyFactory } from '../strategy-types/factory';
+import { StrategyService } from '../strategy.service';
 
 @Component({
   selector: 'app-strategy-performance',
   templateUrl: './strategy-performance.component.html',
   styleUrls: ['./strategy-performance.component.less']
 })
-export class StrategyPerformanceComponent implements OnInit {
-
-  @Input() strategy!: Strategy;
-
+export class StrategyPerformanceComponent {
+  loading: Boolean = true;
+  strategy!: Strategy;
+  stopEngine: Engine | null = null;
   positions?: Position[];
   perfLog?: Metrics[];
 
   constructor(
     private positionService: PositionService,
-  ) { }
+    private strategyService: StrategyService,
+    private engineService: EngineService,
+    private indicatorService: IndicatorService,
+    private route: ActivatedRoute,
+  ) {
+    const strategy_id = Number(this.route.snapshot.paramMap.get('strategy_id'));
 
-  ngOnInit(): void {
-    this.positionService.getPositionsByStrategyId(this.strategy.id).subscribe({
+    this.loading = true;
+
+    this.strategyService.getStrategy(strategy_id).subscribe({
       next: (res) => {
-        this.positions = res;
-        if (this.positions.length > 0) {
-          this.perfLog = this.strategy.getMetrics(this.positions, this.strategy.indicators!);
+        this.strategy = StrategyFactory(res);
+
+        if (res.stop_engine_id) {
+          this.engineService.getEngine(res.stop_engine_id).subscribe({
+            next: (res) => {
+              this.stopEngine = res;
+            }
+          });
         }
+
+        this.positionService.getPositionsByStrategyId(this.strategy.id).subscribe({
+          next: (res) => {
+            this.positions = res;
+            this.setPerfLog()
+          }
+        });
+
+        this.indicatorService.getIndicators(this.strategy.id).subscribe({
+          next: (res) => {
+            this.loading = false;
+            this.strategy.indicators = res;
+            this.setPerfLog()
+          }
+        });
       }
     });
+  }
+
+  setPerfLog() {
+    if (this.positions && this.strategy.indicators) {
+      this.perfLog = this.strategy.getMetrics(this.positions, this.strategy.indicators);
+    }
   }
 
 }
