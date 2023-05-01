62237faaf22ebf356df8b1bb49d9a70af8622c7f
Ricardo Marliere <rbmarliere@gmail.com>
2023-05-01 05:41:11 -0300

    import_tweets

--- scripts/data/import_tweets.py
+++ scripts/data/import_tweets.py
@@ -8,7 +8,7 @@ import sliver.core
 from sliver.strategies.hypnox import HypnoxTweet
 
 
-def main(args):
+def clean(args):
     # id;user;fullname;url;timestamp;replies;likes;retweets;text
     print("reading csv")
     input_df = pandas.read_csv(args.input_file, sep=";", encoding="utf-8")
@@ -31,10 +31,24 @@ def main(args):
             code.interact(local=locals())
 
 
+def main(args):
+    # time	text
+    print("reading csv")
+    input_df = pandas.read_csv(args.input_file, sep="\t", encoding="utf-8")
+    input_df = input_df[["time", "text"]]
+    input_df.time = pandas.to_datetime(input_df.time, utc=True)
+
+    HypnoxTweet.insert_many(input_df.to_dict("records")).execute()
+
+
 if __name__ == "__main__":
     argp = argparse.ArgumentParser()
     argp.add_argument("-i", "--input-file", help="CSV file to import", required=True)
+    argp.add_argument("-c", "--clean", help="Clean the CSV file", action="store_true")
     args = argp.parse_args()
 
     with sliver.core.connection.atomic():
-        main(args)
+        if args.clean:
+            clean(args)
+        else:
+            main(args)
