97fc77d5996bcc4a423f3944d7bac48261961cb0
Ricardo Marliere <rbmarliere@gmail.com>
2023-05-06 10:55:35 -0300

    position refreshing migration

--- migrations/20230505_position_refreshing.py
+++ migrations/20230505_position_refreshing.py
@@ -1,9 +1,12 @@
 from playhouse.migrate import PostgresqlMigrator, migrate
 
 import sliver.core
+from sliver.database import connection, db_init
+
+db_init()
 
 
-migrator = PostgresqlMigrator(sliver.core.connection)
+migrator = PostgresqlMigrator(connection)
 
 migrate(
     migrator.add_column(
